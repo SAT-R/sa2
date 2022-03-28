0db9ca3ed3a57c47ae86329eb5427b99f999e502
Oliver Bell <freshollie@gmail.com>
2022-03-28 09:42:59 +0100

    decompile some reg fuctions

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include "global.h"
+
 extern void sub_8001404(void);
 extern void sub_801A51C(void);
 extern char gUnknown_0300188C;
@@ -10,6 +12,60 @@ extern int gUnknown_030022AC;
 
 extern int gUnknown_03004D90;
 extern char gUnknown_03004D50;
+
+void SetReg5(void) {
+    REG_IF = 4;
+    return;
+}
+
+void SetReg100(void) {
+    REG_IF = 0x100;
+    return;
+}
+
+void SetReg200(void) {
+    REG_IF = 0x200;
+    return;
+}
+
+void SetReg400(void) {
+    REG_IF = 0x400;
+    return;
+}
+
+void SetReg800(void) {
+    REG_IF = 0x800;
+    return;
+}
+
+void SetReg8(void) {
+    REG_IF = 8;
+    return;
+}
+
+void SetReg10(void) {
+    REG_IF = 0x10;
+    return;
+}
+
+void SetReg20(void) {
+    REG_IF = 0x20;
+    return;
+}
+
+void SetReg1000(void) {
+    REG_IF = 0x1000;
+    return;
+}
+
+void SetReg2000(void) {
+    REG_IF = 0x2000;
+    return;
+}
+
+void DummyFunc(void) {
+    return;
+}
 
 void sub_8002450(void) {
   int local_c;
