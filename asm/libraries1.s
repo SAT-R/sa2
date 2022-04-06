be114cd8702c91a752b26b2cd0983cb0f356c168
Oliver Bell <freshollie@gmail.com>
2022-04-07 00:31:26 +0800

    start multi boot decompilation

--- asm/libraries1.s
+++ asm/libraries1.s
@@ -4,8 +4,8 @@
 .syntax unified
 .arm
 
-	thumb_func_start sub_80007E0
-sub_80007E0: @ 0x080007E0
+	thumb_func_start MultiBootMain
+MultiBootMain: @ 0x080007E0
 	push {r4, r5, r6, r7, lr}
 	mov r7, sl
 	mov r6, sb
