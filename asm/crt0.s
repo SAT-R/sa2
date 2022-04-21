43c373276fd1369a1884fbc634d08ce277a6cdc3
Oliver Bell <freshollie@gmail.com>
2022-04-21 15:00:54 +0100

    rely on gbafix for rom header

--- asm/crt0.s
+++ asm/crt0.s
@@ -6,7 +6,42 @@
 
 Init: @ 0x08000000
 	b start_vector
-	.include "asm/rom_header.inc"
+
+	# These will be set by gbafix
+	
+	.global RomHeaderNintendoLogo
+RomHeaderNintendoLogo:
+	.space 0x9c
+
+RomHeaderGameTitle:
+	.space 12
+
+RomHeaderGameCode:
+	.4byte 0
+
+RomHeaderMakerCode:
+	.2byte 0
+
+RomHeaderMagic:
+	.byte 0x96
+
+RomHeaderMainUnitCode:
+	.byte 0
+
+RomHeaderDeviceType:
+	.byte 0
+
+RomHeaderReserved1:
+	.space 7
+
+RomHeaderSoftwareVersion:
+	.byte 0
+
+RomHeaderChecksum:
+	.byte 0
+
+RomHeaderReserved2:
+	.space 2
 
 start_vector:
 	mov r0, #0x12
