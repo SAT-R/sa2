1fa0bbb2f6bebc0bf93d87e4a90af04ae889218f
Oliver Bell <freshollie@gmail.com>
2022-04-24 02:39:45 +0100

    create another work around for oddly ordererd data

--- tools/mid2agb/agb.cpp
+++ tools/mid2agb/agb.cpp
@@ -442,8 +442,7 @@ void PrintAgbTrack(std::vector<Event>& events)
     // the keyshift after the volume event
     bool needsKeyshiftWorkaround =
         events[0].type == EventType::WholeNoteMark &&
-        IsVolumeEvent(events[1]) &&
-        events[2].type == EventType::InstrumentChange;
+        IsVolumeEvent(events[1]);
 
     if (!foundVolBeforeNote)
         PrintByte("\tVOL   , 127*%s_mvl/mxv", g_asmLabel.c_str());
@@ -451,17 +450,14 @@ void PrintAgbTrack(std::vector<Event>& events)
     if (!needsKeyshiftWorkaround) {
         PrintByte("KEYSH , %s_key%+d", g_asmLabel.c_str(), 0);
     }
-
     PrintWait(g_initialWait);
 
     for (unsigned i = 0; events[i].type != EventType::EndOfTrack; i++)
     {
-        const Event& event = events[i];
-
-        // print the keyshift after we have written the volume value
-        if (needsKeyshiftWorkaround && i == 2) {
-            PrintByte("\tKEYSH , %s_key%+d", g_asmLabel.c_str(), 0);
-        }
+        // We are going to be hacky
+        // and modify the time value, so can't be const
+        Event& event = events[i];
+        std::int32_t eventTime = event.time;
 
         if (IsPatternBoundary(event.type))
         {
@@ -528,7 +524,19 @@ void PrintAgbTrack(std::vector<Event>& events)
             PrintOp(event.time, "BEND  ", "c_v%+d", event.param2 - 64);
             break;
         case EventType::Controller:
+            // Ensure that a wait value is not printed after the volume
+            // command
+            if (needsKeyshiftWorkaround && i == 1) {
+                event.time = 0;
+            }
+
             PrintControllerOp(event);
+            // print the keyshift and any wait after we have written the volume value
+            if (needsKeyshiftWorkaround && i == 1) {
+                PrintByte("\tKEYSH , %s_key%+d", g_asmLabel.c_str(), 0);
+                PrintWait(eventTime);
+                event.time = eventTime;
+            }
             break;
         default:
             PrintWait(event.time);
