846389df1f3fdc3611814920193eb57ba8b719e1
Oliver Bell <freshollie@gmail.com>
2022-05-02 18:11:53 +0100

    add script to try and match midi time signatures

--- scripts/match_next_midi.py
+++ scripts/match_next_midi.py
@@ -10,6 +10,17 @@ LDSCRIPT = 'ldscript.txt'
 
 to_match = reversed(sorted(map(lambda s: s.split('.s')[0], filter(lambda f: f.endswith('.s') , os.listdir(SONGS_DIR)))))
 
+all_time_signatures = [None]
+
+for denom in range(2, 17):
+    if denom % 2 > 0:
+        continue
+
+    for num in range(1, 17):
+        ts = (num, denom)
+        if ts not in all_time_signatures:
+            all_time_signatures.append(ts)
+
 for next_song in to_match:
     if next_song in skipped:
         print(f"skipping {next_song}")
@@ -33,13 +44,24 @@ for next_song in to_match:
         ldscript.write(ldscript_data)
 
 
-    os.system("make tidy >/dev/null 2>&1")
-    print('Verifying build')
-    result = os.system("make >/dev/null 2>&1")
-    if not result:
-        print('success!')
-        os.system('git add -A')
-    else:
+    print('Cleaning build')
+    os.system('make tidy >/dev/null 2>&1')
+
+    success = False
+    for ts in all_time_signatures:
+        if ts:
+            print(f'applying time signature {ts[0]}/{ts[1]}')
+            os.system(f'python3 scripts/edit_time_signature.py {next_song} {ts[0]} {ts[1]}')
+        
+        print('Verifying build')
+        error = os.system("make >/dev/null 2>&1")
+        if not error:
+            print('success!')
+            os.system('git add -A')
+            success = True
+            break
+
+    if not success:      
         print('fail, reverting')
         os.system('git checkout -- sound ldscript.txt')
         os.system('git clean -f >/dev/null 2>&1')
