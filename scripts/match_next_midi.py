1b8a4b30218292b27f565da57676fa00b16c0d33
Oliver Bell <freshollie@gmail.com>
2022-05-04 02:25:42 +0100

    clean ups after finishing midis

--- scripts/match_next_midi.py
+++ scripts/match_next_midi.py
@@ -10,21 +10,7 @@ LDSCRIPT = 'ldscript.txt'
 
 to_match = reversed(sorted(map(lambda s: s.split('.s')[0], filter(lambda f: f.endswith('.s') , os.listdir(SONGS_DIR)))))
 
-all_time_signatures = [None]
-
-for denom in range(2, 17):
-    if denom % 2 > 0:
-        continue
-
-    for num in range(1, 17):
-        ts = (num, denom)
-        if ts not in all_time_signatures:
-            all_time_signatures.append(ts)
-
 for next_song in to_match:
-    if next_song != "song0020":
-        continue
-
     if next_song in skipped:
         print(f"skipping {next_song}")
         continue
@@ -46,26 +32,15 @@ for next_song in to_match:
     with open(LDSCRIPT, "w") as ldscript:
         ldscript.write(ldscript_data)
 
-
     print('Cleaning build')
     os.system('make tidy >/dev/null 2>&1')
 
-    success = False
-    for ts in all_time_signatures:
-        if ts:
-            print(f'applying time signature {ts[0]}/{ts[1]}')
-            os.system(f'python3 scripts/edit_time_signature.py {next_song} {ts[0]} {ts[1]}')
-        
-        print('Verifying build')
-        error = os.system("make >/dev/null 2>&1")
-        if not error:
-            print('success!')
-            os.system('git add -A')
-            success = True
-            break
-        break
-
-    if not success:      
+    print('Verifying build')
+    error = os.system("make >/dev/null 2>&1")
+    if not error:
+        print('success!')
+        os.system('git add -A')
+    else:      
         print('fail, reverting')
         os.system('git checkout -- sound ldscript.txt')
         os.system('git clean -f >/dev/null 2>&1')
--- sound/songs/midi/non_matching/NOTES.md
+++ sound/songs/midi/non_matching/NOTES.md
@@ -1,2 +1,1 @@
-* song0020: line 5627 (of compiled) doesn't reuse the previous pattern even though it's identical. Need to understand how mid2agb detects these
 * song0291: needs something to change at tick 24? The song is possibly unmatchable as a midi
