f302956109b83aa1f040569c83f437a6a4af58ae
Oliver Bell <freshollie@gmail.com>
2022-07-12 00:33:28 +0000

    move structs back to profile

--- src/profile.c
+++ src/profile.c
@@ -16,6 +16,199 @@
 #include "ui.h"
 #include "time.h"
 
+#define NUM_PLAYER_DATA_MENU_ITEMS 4
+
+struct TimeRecordDisplay {
+    struct UNK_0808B3FC_UNK240 deliminators[2];
+    struct UNK_0808B3FC_UNK240 minute;
+    struct UNK_0808B3FC_UNK240 seconds[2];
+    struct UNK_0808B3FC_UNK240 millis[2];
+};
+
+struct TimeRecordsScreen {
+    struct UNK_802D4CC_UNK270 unk0;
+    struct Unk_03002400 coursesViewBackground;
+    struct UNK_0808B3FC_UNK240 choiceViewItemsOrZoneTitle[2];
+    struct UNK_0808B3FC_UNK240 actTitle[2];
+    struct UNK_0808B3FC_UNK240 choiceViewTitleOrZoneSubtitle;
+    struct UNK_0808B3FC_UNK240 choiceViewScrollArrows[4];
+
+    struct PlayerDataMenu* playerDataMenu;
+
+    struct TimeRecords* timeRecords;
+
+    struct Unk_03002400 coursesViewCharacterBackground;
+    struct Unk_03002400 coursesViewCharacter;
+    struct UNK_0808B3FC_UNK240 timeRecordDisplays[3];
+    struct TimeRecordDisplay timeDisplays[3];
+
+    u8 character;
+
+    u8 zone;
+    u8 act;
+
+    u8 animFrame;
+    bool8 unusedUnk708;
+    u8 availableCharacters;
+    u8 unlockedCourses[NUM_CHARACTERS];
+    u8 language;
+
+    bool8 isBossMode;
+
+    u8 view;
+}; /* size 0x714 */
+
+struct LanguageScreen {
+    struct UNK_0808B3FC_UNK240 headerFooter[2];
+    struct UNK_0808B3FC_UNK240 languageOptions[NUM_LANGUAGES];
+    struct UNK_0808B3FC_UNK240 optionOutline;
+    struct Unk_03002400 unk1B0;
+    struct UNK_802D4CC_UNK270 unk1F0;
+
+    struct OptionsScreen* optionsScreen;
+
+    s8 menuCursor;
+
+    bool8 creatingNewProfile;
+}; /* size 0x204 */
+
+struct NameInputDisplay {
+    struct UNK_0808B3FC_UNK240 displayCursor;
+    struct UNK_0808B3FC_UNK240 characterDisplay[MAX_PLAYER_NAME_LENGTH];
+
+    u16 unk150;
+    u16 cursor;
+
+    u16 buffer[MAX_PLAYER_NAME_LENGTH];
+};
+
+struct ProfileNameScreen {
+    struct UNK_0808B3FC_UNK240 title;
+    struct UNK_0808B3FC_UNK240 controls[3];
+    struct Unk_03002400 background;
+    struct Unk_03002400 charMatrixBackground;
+    
+    struct UNK_802D4CC_UNK270 unk140;
+    struct PlayerDataMenu* playerDataMenu;
+
+    struct UNK_0808B3FC_UNK240 focusedCell[2];
+    struct UNK_0808B3FC_UNK240 scrollArrows[2];
+    struct Unk_03002400 charMatrix;
+    u16 matrixPageIndex;
+
+    s8 cursorCol;
+    s8 cursorRow;
+
+    u8 filler254[4];
+    
+    struct NameInputDisplay nameInput;
+
+    u16 matrixCursorIndex;
+    u8 language;
+    u8 onCompleteAction;
+}; /* size 0x3BC */
+
+struct PlayerDataMenu {
+    struct UNK_0808B3FC_UNK240 headerFooter[2];
+    struct UNK_0808B3FC_UNK240 menuItems[NUM_PLAYER_DATA_MENU_ITEMS];
+    struct UNK_0808B3FC_UNK240 menuItemOutline;
+
+    struct UNK_802D4CC_UNK270 unk150;
+    
+    struct OptionsScreen* optionsScreen;
+    s8 menuCursor;
+    s8 animFrame;
+
+    s8 language;
+    
+    s8 state;
+}; /* size 0x164 */
+
+struct SwitchMenu {
+    struct OptionsScreen* optionsScreen;
+    struct UNK_0808B3FC_UNK240 headerFooter[2];
+    struct UNK_0808B3FC_UNK240 options[2];
+    struct UNK_0808B3FC_UNK240 switchValueOutline;
+    s8 switchValue;
+    s8 animFrame;
+    s8 language;
+}; /* size 0xF8 */
+
+struct ButtonConfigMenu {
+    struct OptionsScreen* optionsScreen;
+    struct UNK_0808B3FC_UNK240 staticElements[6];
+    struct UNK_0808B3FC_UNK240 buttonActions[3];
+    struct UNK_0808B3FC_UNK240 scrollArrows[2];
+    struct UNK_0808B3FC_UNK240 controlFocus;
+    u8 aButtonAction;
+    u8 bButtonAction;
+    u8 rShoulderAction;
+    u8 focus;
+    s8 animFrame;
+    s8 language;
+}; /* size 0x24C */
+
+struct DeleteScreen {
+    struct UNK_0808B3FC_UNK240 headerFooter[2];
+    struct UNK_0808B3FC_UNK240 options[2];
+    struct UNK_0808B3FC_UNK240 optionOutline;
+    struct Unk_03002400 background;
+    struct UNK_802D4CC_UNK270 unk130;
+    struct OptionsScreen* optionsScreen; 
+    s8 confirmationCursor;
+    bool8 unusedUnk141;
+    s8 language;
+    s8 deleteConfirmed;
+}; /* 0x144 */
+
+struct MultiplayerRecordRow {
+    u16 playerName[6];
+
+    bool8 slotFilled;
+    u8 wins;
+    u8 loses;
+    u8 draws;
+    struct UNK_0808B3FC_UNK240 nameDisplay[6];
+    struct UNK_0808B3FC_UNK240 winsDigits[2];
+    struct UNK_0808B3FC_UNK240 losesDigits[2];
+    struct UNK_0808B3FC_UNK240 defeatsDigits[2];
+}; /* size 0x250 */
+
+struct MultiplayerRecordsTable {
+   struct MultiplayerRecordRow rows[NUM_MULTIPLAYER_SCORES];
+}; /* size 0x1720 */
+
+struct MultiplayerRecordsScreen {
+    struct UNK_802D4CC_UNK270 unk0;
+
+    struct Unk_03002400 backgroundTrims;
+    struct Unk_03002400 background;
+
+    struct UNK_0808B3FC_UNK240 title;
+    struct UNK_0808B3FC_UNK240 columnHeaders;
+    struct UNK_0808B3FC_UNK240 scrollArrows[2];
+    struct UNK_0808B3FC_UNK240 playerNameDisplay[6];
+    struct UNK_0808B3FC_UNK240 playerWinsDigits[2];
+    struct UNK_0808B3FC_UNK240 playerLosesDigits[2];
+    struct UNK_0808B3FC_UNK240 playerDrawsDigits[2];
+
+    struct PlayerDataMenu* playerDataMenu;
+
+    struct MultiplayerRecordsTable* table;
+    u8 scrollAnimFrame;
+    u8 playerWins;
+    u8 playerLoses;
+    u8 playerDraws;
+
+    u16 playerName[6];
+
+    u8 targetFirstVisibleRowIndex;
+    u8 currentFirstVisibleRowIndex;
+    u8 scrollIndex;
+
+    u8 language;
+}; /* size 0x3A8 */
+
 // vram addresses
 void* gProfileScreenNextVramAddress;
 void* gProfileScreenSubMenuNextVramAddress;
