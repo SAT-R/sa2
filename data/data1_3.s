cb4bd1566e68ca2f65c22019a913d6017885c578
Oliver Bell <freshollie@gmail.com>
2022-07-19 21:38:18 +0100

    more math

---
+++ data/data1_3.s
@@ -0,0 +1,53 @@
+    .section .rodata
+
+    .global gUnknown_080E0294
+gUnknown_080E0294:
+    .incbin "baserom.gba", 0x000E0294, 0x6
+
+    .global gUnknown_080E029A
+gUnknown_080E029A:
+    .incbin "baserom.gba", 0x000E029A, 0x10
+
+    .global gUnknown_080E02AA
+gUnknown_080E02AA:
+    .incbin "baserom.gba", 0x000E02AA, 0xE
+
+    .global gUnknown_080E02B8
+gUnknown_080E02B8:
+    .incbin "baserom.gba", 0x000E02B8, 0x1C
+
+    .global gUnknown_080E02D4
+gUnknown_080E02D4:
+    .incbin "baserom.gba", 0x000E02D4, 0x8
+
+    .global gUnknown_080E02DC
+gUnknown_080E02DC:
+    .incbin "baserom.gba", 0x000E02DC, 0x10
+
+    .global gUnknown_080E02EC
+gUnknown_080E02EC:
+    .incbin "baserom.gba", 0x000E02EC, 0x90
+
+    .global gUnknown_080E037C
+gUnknown_080E037C:
+    .incbin "baserom.gba", 0x000E037C, 0x8
+
+    .global gUnknown_080E0384
+gUnknown_080E0384:
+    .incbin "baserom.gba", 0x000E0384, 0xF0
+
+    .global gUnknown_080E0474
+gUnknown_080E0474:
+    .incbin "baserom.gba", 0x000E0474, 0x50
+
+    .global gUnknown_080E04C4
+gUnknown_080E04C4:
+    .incbin "baserom.gba", 0x000E04C4, 0x10
+
+    .global gUnknown_080E04D4
+gUnknown_080E04D4:
+    .incbin "baserom.gba", 0x000E04D4, 0xF0
+
+    .global gUnknown_080E05C4
+gUnknown_080E05C4:
+    .incbin "baserom.gba", 0x000E05C4, 0x20
