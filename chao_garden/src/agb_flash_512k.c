6715e378c8036455a1cdd8597e92eec89293e48b
JaceCear <nix@BFG123LQXRV.localdomain>
2022-11-14 22:16:43 +0100

    Insert flash lib into Tiny Chao Garden

---
+++ chao_garden/src/agb_flash_512k.c
@@ -0,0 +1,76 @@
+#include "global.h"
+#include "gba/flash_internal.h"
+
+static const char AgbLibFlash512KVersion[] = "FLASH512_V130";
+const struct FlashSetupInfo * const gSetup512KInfos[] = {
+    &LE39FW512,
+    &MN63F805MNP,
+    &MX29L512, 
+    &DefaultFlash512K
+};
+
+u16 IdentifyFlash(void)
+{
+    u16 result;
+    u16 flashId;
+    const struct FlashSetupInfo * const *setupInfo;
+
+    REG_WAITCNT = (REG_WAITCNT & ~WAITCNT_SRAM_MASK) | WAITCNT_SRAM_8;
+
+    flashId = ReadFlashId();
+
+    setupInfo = gSetup512KInfos;
+    result = 1;
+
+    for (;;)
+    {
+        if ((*setupInfo)->type.ids.separate.makerId == 0)
+            break;
+
+        if (flashId == (*setupInfo)->type.ids.joined)
+        {
+            result = 0;
+            break;
+        }
+
+        setupInfo++;
+    }
+
+    ProgramFlashSector = (*setupInfo)->programFlashSector;
+    EraseFlashChip = (*setupInfo)->eraseFlashChip;
+    EraseFlashSector = (*setupInfo)->eraseFlashSector;
+    WaitForFlashWrite = (*setupInfo)->WaitForFlashWrite;
+    gFlashMaxTime = (*setupInfo)->maxTime;
+    gFlash = &(*setupInfo)->type;
+
+    return result;
+}
+
+u16 WaitForFlashWrite512K_Common(u8 phase, u8 *addr, u8 lastData)
+{
+    u16 result = 0;
+    u8 status;
+
+    StartFlashTimer(phase);
+
+    while ((status = PollFlashStatus(addr)) != lastData)
+    {
+        if (gFlashTimeoutFlag)
+        {
+            if (PollFlashStatus(addr) == lastData)
+                break;
+
+            if (gFlash->ids.separate.makerId == 0xc2)
+                FLASH_WRITE(0x5555, 0xF0);
+
+            result = phase | 0xC000u;
+            break;
+        }
+    }
+
+    StopFlashTimer();
+
+    return result;
+}
+
+
