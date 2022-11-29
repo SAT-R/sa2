1bac9514802692d94a0874932a54e645653391a5
JaceCear <11230293+JaceCear@users.noreply.github.com>
2022-11-29 02:51:38 +0100

    Match Task_805E02C

--- src/interactable_spring_bouncy.c
+++ src/interactable_spring_bouncy.c
@@ -167,7 +167,7 @@ void Task_805E02C()
     register struct UNK_0808B3FC_UNK240 *displayed = &spring->displayed;
     Interactable *ia = spring->base.ia;
     s32 screenX, screenY;
-    register u8 r8/* asm("r8")*/;
+    u8 r8;
 
     screenX = SpriteGetScreenPos(spring->base.spriteX, spring->base.regionX);
     screenY = SpriteGetScreenPos(ia->y, spring->base.regionY);
@@ -186,26 +186,23 @@ void Task_805E02C()
 
             if (LEVEL_TO_ZONE(gCurrentLevel) == ZONE_3) {
                 displayed->unkA = SA2_ANIM_570;
-                spring->displayed.unk20 = r8;
-                spring->displayed.unk21 = 0xFF;
+                displayed->unk20 = r8;
             } else if (LEVEL_TO_ZONE(gCurrentLevel) == ZONE_6) {
                 displayed->unkA = SA2_ANIM_603;
-                spring->displayed.unk20 = r8;
-                spring->displayed.unk21 = 0xFF;
+                displayed->unk20 = r8;
             } else {
                 displayed->unkA = SA2_ANIM_SPRING_BOUNCY;
-                spring->displayed.unk20 = r8;
-                spring->displayed.unk21 = 0xFF;
+                displayed->unk20 = r8;
             }
+            displayed->unk21 = -1;
         }
         // _805E0F4
         sub_80051E8(displayed);
     }
 }
 
-#if 0 // Matches
-void TaskDestructor_Interactable_BouncySpring(struct Task *t) {
+void TaskDestructor_Interactable_BouncySpring(struct Task *t)
+{
     Sprite_BouncySpring *spring = TaskGetStructPtr(t);
     VramFree(spring->displayed.unk4);
 }
-#endif
