ca888f537cf64d75d74627c9186f0ee96e881622
JaceCear <11230293+JaceCear@users.noreply.github.com>
2022-12-05 00:40:31 +0100

    Add enemy indices

---
+++ include/constants/enemies.h
@@ -0,0 +1,28 @@
+#ifndef GUARD_CONSTANTS_ENEMIES_H
+#define GUARD_CONSTANTS_ENEMIES_H
+
+#define ENEMY__MON               0  // Jumping monkey
+#define ENEMY__KIKI              1  // Monkey on tree
+#define ENEMY__BUZZER            2
+#define ENEMY__GOHLA             3
+#define ENEMY__KURAKURA          4
+#define ENEMY__KUBINAGA          5
+#define ENEMY__PIKOPIKO          6
+#define ENEMY__BELL              7
+#define ENEMY__YADO              8
+#define ENEMY__CIRCUS            9
+#define ENEMY__KOURA            10 /* Should have a flag to enable "fly-mode" */
+#define ENEMY__MADILLO          11
+#define ENEMY__STRAW            12
+#define ENEMY__HAMMERHEAD       13
+#define ENEMY__SPINNER          14
+#define ENEMY__MOUSE            15
+#define ENEMY__PEN              16
+#define ENEMY__GEJIGEJI         17
+#define ENEMY__BALLOON          18
+#define ENEMY__FLICKEY          19
+#define ENEMY__KYURA            20
+#define ENEMY__STAR             21
+#define ENEMY__BULLETBUZZER     22
+
+#endif // GUARD_CONSTANTS_ENEMIES_H
