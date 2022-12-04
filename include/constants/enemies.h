5c139105cba0a1e48f5ed6d57bf64875f6314236
JaceCear <11230293+JaceCear@users.noreply.github.com>
2022-12-05 00:59:23 +0100

    Add some descriptions

--- include/constants/enemies.h
+++ include/constants/enemies.h
@@ -1,28 +1,28 @@
 #ifndef GUARD_CONSTANTS_ENEMIES_H
 #define GUARD_CONSTANTS_ENEMIES_H
 
-#define ENEMY__MON               0  // Jumping monkey
-#define ENEMY__KIKI              1  // Monkey on tree
-#define ENEMY__BUZZER            2
-#define ENEMY__GOHLA             3
-#define ENEMY__KURAKURA          4
-#define ENEMY__KUBINAGA          5
-#define ENEMY__PIKOPIKO          6
-#define ENEMY__BELL              7
-#define ENEMY__YADO              8
-#define ENEMY__CIRCUS            9
-#define ENEMY__KOURA            10 /* Should have a flag to enable "fly-mode" */
-#define ENEMY__MADILLO          11
-#define ENEMY__STRAW            12
-#define ENEMY__HAMMERHEAD       13
-#define ENEMY__SPINNER          14
-#define ENEMY__MOUSE            15
-#define ENEMY__PEN              16
-#define ENEMY__GEJIGEJI         17
-#define ENEMY__BALLOON          18
-#define ENEMY__FLICKEY          19
-#define ENEMY__KYURA            20
-#define ENEMY__STAR             21
-#define ENEMY__BULLETBUZZER     22
+#define ENEMY__MON          0 // Jumping monkey
+#define ENEMY__KIKI         1 // Monkey on tree
+#define ENEMY__BUZZER       2
+#define ENEMY__GOHLA        3
+#define ENEMY__KURAKURA     4
+#define ENEMY__KUBINAGA     5
+#define ENEMY__PIKOPIKO     6 // Hammer Clown
+#define ENEMY__BELL         7
+#define ENEMY__YADO         8
+#define ENEMY__CIRCUS       9 // Monkey with explosive
+#define ENEMY__KOURA        10 // Turtle with spring on its back, can have rocket engines.
+#define ENEMY__MADILLO      11
+#define ENEMY__STRAW        12 // Bird
+#define ENEMY__HAMMERHEAD   13 // Shark-shaped platform
+#define ENEMY__SPINNER      14
+#define ENEMY__MOUSE        15
+#define ENEMY__PEN          16 // Penguin
+#define ENEMY__GEJIGEJI     17
+#define ENEMY__BALLOON      18 // Flying Squid
+#define ENEMY__FLICKEY      19
+#define ENEMY__KYURA        20
+#define ENEMY__STAR         21
+#define ENEMY__BULLETBUZZER 22 // A Buzzer with Jet-Engines
 
 #endif // GUARD_CONSTANTS_ENEMIES_H
