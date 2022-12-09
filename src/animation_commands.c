3b4fdec53acf0b55067aa045a9b9afc1f86d314e
JaceCear <11230293+JaceCear@users.noreply.github.com>
2022-12-09 03:59:00 +0100

    Formatting

--- src/animation_commands.c
+++ src/animation_commands.c
@@ -12,9 +12,11 @@ extern int animCmd_GetTileIndex(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *
 static int animCmd_GetPalette(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
 static int animCmd_JumpBack(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
 static int animCmd_4(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
-static int animCmd_PlaySoundEffect(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
+static int animCmd_PlaySoundEffect(s32 *variantCursor,
+                                   struct UNK_0808B3FC_UNK240 *sprite);
 extern int animCmd_6(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
-static int animCmd_TranslateSprite(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
+static int animCmd_TranslateSprite(s32 *variantCursor,
+                                   struct UNK_0808B3FC_UNK240 *sprite);
 static int animCmd_8(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
 static int animCmd_9(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
 static int animCmd_10(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
@@ -43,15 +45,15 @@ extern u16 gObjPalette[];
 extern u32 gFlags;
 
 // (-2)
-int animCmd_GetPalette(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
+int animCmd_GetPalette(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+{
     sprite->unk14 += 3;
 
-    if(!(sprite->unk10 & 0x40000)) {
+    if (!(sprite->unk10 & 0x40000)) {
         s32 paletteIndex = variantCursor[1];
         u16 count, insertIndex;
 
-        DmaCopy32(3,
-                  &gUnknown_03002794->palettes[paletteIndex * 16],
+        DmaCopy32(3, &gUnknown_03002794->palettes[paletteIndex * 16],
                   &gObjPalette[sprite->focused * 16 + ((u16 *)variantCursor)[5]],
                   ((u16 *)variantCursor)[4] * 2);
 
@@ -62,21 +64,24 @@ int animCmd_GetPalette(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
 }
 
 // (-3)
-int animCmd_JumpBack(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
+int animCmd_JumpBack(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+{
     sprite->unk14 -= variantCursor[1];
 
     return 1;
 }
 
 // (-4)
-int animCmd_4(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
+int animCmd_4(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+{
     sprite->unk10 |= 0x4000;
 
     return 0;
 }
 
 // (-5)
-int animCmd_PlaySoundEffect(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
+int animCmd_PlaySoundEffect(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+{
     sprite->unk14 += 2;
 
     m4aSongNumStart(((u16 *)variantCursor)[2]);
@@ -85,7 +90,8 @@ int animCmd_PlaySoundEffect(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *spri
 }
 
 // (-7)
-int animCmd_TranslateSprite(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
+int animCmd_TranslateSprite(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+{
     sprite->unk14 += 2;
 
     sprite->x += ((u16 *)variantCursor)[2];
@@ -95,14 +101,16 @@ int animCmd_TranslateSprite(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *spri
 }
 
 // (-8)
-int animCmd_8(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
+int animCmd_8(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+{
     sprite->unk14 += 3;
 
     return 1;
 }
 
 // (-9)
-int animCmd_9(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
+int animCmd_9(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+{
     sprite->unk14 += 2;
 
     sprite->anim = ((AnimId *)variantCursor)[2];
@@ -113,7 +121,8 @@ int animCmd_9(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
 }
 
 // (-10)
-int animCmd_10(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
+int animCmd_10(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+{
     sprite->unk14 += 4;
 #ifdef UBFIX
     return 1; // I think this should be the correct behavior?
@@ -123,7 +132,8 @@ int animCmd_10(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
 }
 
 // (-11)
-int animCmd_11(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
+int animCmd_11(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+{
     sprite->unk14 += 2;
 
     sprite->unk10 = (sprite->unk10 & ~0x3000) | (variantCursor[1] << 12);
@@ -132,7 +142,8 @@ int animCmd_11(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
 }
 
 // (-12)
-int animCmd_12(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite) {
+int animCmd_12(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+{
     sprite->unk14 += 2;
     sprite->unk1A = variantCursor[1] << 6;
 
