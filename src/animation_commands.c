98ff094c3afbea5f91b02a812d74e653dbf56f08
JaceCear <11230293+JaceCear@users.noreply.github.com>
2022-12-09 20:50:40 +0100

    Use one struct per command

--- src/animation_commands.c
+++ src/animation_commands.c
@@ -5,23 +5,26 @@
 #include "flags.h"
 #include "sprite.h"
 
-typedef int (*AnimationCommandFunc)(s32 *variantCursor,
-                                    struct UNK_0808B3FC_UNK240 *sprite);
+#include "animation_commands.h"
 
-extern int animCmd_GetTileIndex(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
-static int animCmd_GetPalette(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
-static int animCmd_JumpBack(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
-static int animCmd_4(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
-static int animCmd_PlaySoundEffect(s32 *variantCursor,
-                                   struct UNK_0808B3FC_UNK240 *sprite);
-extern int animCmd_6(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
-static int animCmd_TranslateSprite(s32 *variantCursor,
-                                   struct UNK_0808B3FC_UNK240 *sprite);
-static int animCmd_8(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
-static int animCmd_9(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
-static int animCmd_10(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
-static int animCmd_11(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
-static int animCmd_12(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite);
+typedef s32 (*AnimationCommandFunc)(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+
+extern s32 animCmd_GetTileIndex(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+static s32 animCmd_GetPalette(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+static s32 animCmd_JumpBack(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+static s32 animCmd_4(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+static s32 animCmd_PlaySoundEffect(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+extern s32 animCmd_6(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+static s32 animCmd_TranslateSprite(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+static s32 animCmd_8(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+static s32 animCmd_SetIdAndVariant(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+s32 animCmd_10(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+static s32 animCmd_11(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+static s32 animCmd_12(void *cursor, struct UNK_0808B3FC_UNK240 *sprite);
+
+extern struct SpriteTables *gUnknown_03002794;
+extern u16 gObjPalette[];
+extern u32 gFlags;
 
 // make static
 const AnimationCommandFunc animCmdTable[] = {
@@ -34,28 +37,24 @@ const AnimationCommandFunc animCmdTable[] = {
     animCmd_6,
     animCmd_TranslateSprite,
     animCmd_8,
-    animCmd_9,
+    animCmd_SetIdAndVariant,
     animCmd_10,
     animCmd_11,
     animCmd_12,
 };
 
-extern struct SpriteTables *gUnknown_03002794;
-extern u16 gObjPalette[];
-extern u32 gFlags;
-
 // (-2)
-int animCmd_GetPalette(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+s32 animCmd_GetPalette(void *cursor, struct UNK_0808B3FC_UNK240 *sprite)
 {
-    sprite->unk14 += 3;
+    ACmd_GetPalette *cmd = (ACmd_GetPalette *)cursor;
+    sprite->unk14 += AnimCommandSizeInWords(ACmd_GetPalette);
 
     if (!(sprite->unk10 & 0x40000)) {
-        s32 paletteIndex = variantCursor[1];
-        u16 count, insertIndex;
+        s32 paletteIndex = cmd->palId;
 
         DmaCopy32(3, &gUnknown_03002794->palettes[paletteIndex * 16],
-                  &gObjPalette[sprite->focused * 16 + ((u16 *)variantCursor)[5]],
-                  ((u16 *)variantCursor)[4] * 2);
+                  &gObjPalette[sprite->focused * 16 + cmd->insertOffset],
+                  cmd->numColors * 2);
 
         gFlags |= FLAGS_UPDATE_SPRITE_PALETTES;
     }
@@ -64,15 +63,17 @@ int animCmd_GetPalette(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
 }
 
 // (-3)
-int animCmd_JumpBack(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+s32 animCmd_JumpBack(void *cursor, struct UNK_0808B3FC_UNK240 *sprite)
 {
-    sprite->unk14 -= variantCursor[1];
+    ACmd_JumpBack *cmd = cursor;
+    sprite->unk14 -= cmd->offset;
 
     return 1;
 }
 
 // (-4)
-int animCmd_4(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+// Command "End"?
+s32 animCmd_4(void *cursor, struct UNK_0808B3FC_UNK240 *sprite)
 {
     sprite->unk10 |= 0x4000;
 
@@ -80,72 +81,81 @@ int animCmd_4(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
 }
 
 // (-5)
-int animCmd_PlaySoundEffect(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+s32 animCmd_PlaySoundEffect(void *cursor, struct UNK_0808B3FC_UNK240 *sprite)
 {
-    sprite->unk14 += 2;
+    ACmd_PlaySoundEffect *cmd = cursor;
+    sprite->unk14 += AnimCommandSizeInWords(ACmd_PlaySoundEffect);
 
-    m4aSongNumStart(((u16 *)variantCursor)[2]);
+    m4aSongNumStart(cmd->songId);
 
     return 1;
 }
 
 // (-7)
-int animCmd_TranslateSprite(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+s32 animCmd_TranslateSprite(void *cursor, struct UNK_0808B3FC_UNK240 *sprite)
 {
-    sprite->unk14 += 2;
+    ACmd_TranslateSprite *cmd = cursor;
+    sprite->unk14 += AnimCommandSizeInWords(ACmd_TranslateSprite);
 
-    sprite->x += ((u16 *)variantCursor)[2];
-    sprite->y += ((u16 *)variantCursor)[3];
+    sprite->x += cmd->x;
+    sprite->y += cmd->y;
 
     return 1;
 }
 
 // (-8)
-int animCmd_8(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+s32 animCmd_8(void *cursor, struct UNK_0808B3FC_UNK240 *sprite)
 {
-    sprite->unk14 += 3;
+    ACmd_8 *cmd = cursor;
+    sprite->unk14 += AnimCommandSizeInWords(ACmd_8);
 
     return 1;
 }
 
 // (-9)
-int animCmd_9(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+s32 animCmd_SetIdAndVariant(void *cursor, struct UNK_0808B3FC_UNK240 *sprite)
 {
-    sprite->unk14 += 2;
+    ACmd_SetIdAndVariant *cmd = cursor;
+    sprite->unk14 += AnimCommandSizeInWords(ACmd_SetIdAndVariant);
 
-    sprite->anim = ((AnimId *)variantCursor)[2];
+    sprite->anim = cmd->animId;
     sprite->unk21 = 0xFF;
-    sprite->variant = ((AnimId *)variantCursor)[3];
+    sprite->variant = cmd->variant;
 
     return -1;
 }
 
 // (-10)
-int animCmd_10(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+s32 animCmd_10(void *cursor, struct UNK_0808B3FC_UNK240 *sprite)
 {
-    sprite->unk14 += 4;
+    ACmd_10 *cmd = cursor;
+    sprite->unk14 += AnimCommandSizeInWords(ACmd_10);
+
 #ifdef UBFIX
     return 1; // I think this should be the correct behavior?
 #else
-    return (int)variantCursor;
+    return (s32)cursor;
 #endif
 }
 
 // (-11)
-int animCmd_11(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+s32 animCmd_11(void *cursor, struct UNK_0808B3FC_UNK240 *sprite)
 {
-    sprite->unk14 += 2;
+    ACmd_11 *cmd = cursor;
+    sprite->unk14 += AnimCommandSizeInWords(ACmd_11);
 
-    sprite->unk10 = (sprite->unk10 & ~0x3000) | (variantCursor[1] << 12);
+    sprite->unk10 = ((sprite->unk10 & ~0x3000) | (cmd->unk4 << 3 * 4));
 
     return 1;
 }
 
 // (-12)
-int animCmd_12(s32 *variantCursor, struct UNK_0808B3FC_UNK240 *sprite)
+s32 animCmd_12(void *cursor, struct UNK_0808B3FC_UNK240 *sprite)
 {
-    sprite->unk14 += 2;
-    sprite->unk1A = variantCursor[1] << 6;
+    ACmd_12 *cmd = cursor;
+
+    sprite->unk14 += AnimCommandSizeInWords(ACmd_12);
+    sprite->unk1A = cmd->unk4 << 6;
 
     return 1;
 }
