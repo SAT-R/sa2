ab2b9214962c48a8ef02ec38ae264a9afe179155
Oliver Bell <freshollie@gmail.com>
2022-12-19 10:31:51 +0000

    build graphics as part of pipeline

--- scripts/anims/structure_tiles.py
+++ scripts/anims/structure_tiles.py
@@ -1,9 +1,14 @@
 import os
 
-
 for file in os.listdir("graphics/obj_tiles"):
     if file.endswith(".png"):
         anim_segment = file.split("_")[0]
-        
         id = int(anim_segment.split("a")[1])
-        print(id)
+        
+        folder = f"graphics/obj_tiles/4bpp/anim_{id:04}"
+        
+        os.makedirs(folder, exist_ok=True)
+        
+        os.rename(f"graphics/obj_tiles/{file}", f"{folder}/{file.replace(anim_segment + '_', '')}")
+        print(f"graphics/obj_tiles/{file}", f"{folder}/{file.replace(anim_segment + '_', '')}")
+
