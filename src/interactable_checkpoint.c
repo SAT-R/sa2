a55a6a4450b5820ae138cdaf46e1cba4d3105b70
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-01-21 22:57:59 +0100

    Match Task_Interactable_Toggle_Checkpoint

--- src/interactable_checkpoint.c
+++ src/interactable_checkpoint.c
@@ -9,6 +9,7 @@
 #include "sprite.h"
 
 #include "constants/animations.h"
+#include "constants/move_states.h"
 #include "constants/songs.h"
 
 typedef struct {
@@ -28,9 +29,47 @@ typedef struct {
 extern void Task_Interactable_Checkpoint(void);
 extern void Task_Interactable_Toggle_Checkpoint(void);
 
+extern u32 gUnknown_030053E4;
+extern u32 gUnknown_080D63FC[34][2];
 
+// const static TileInfoCheckpoint sCheckpointTileInfo[8] = { {} };
 
-//const static TileInfoCheckpoint sCheckpointTileInfo[8] = { {} };
+void Task_Interactable_Toggle_Checkpoint(void)
+{
+
+    Sprite_Toggle_Checkpoint *toggle = TaskGetStructPtr(gCurTask);
+    Interactable *ia = toggle->base.ia;
+    s32 posX, posY;
+    s16 screenX, screenY;
+
+    posX = SpriteGetScreenPos(toggle->base.spriteX, toggle->base.regionX);
+    posY = SpriteGetScreenPos(ia->y, toggle->base.regionY);
+
+    screenX = (posX - gCamera.x);
+    screenY = (posY - gCamera.y);
+
+    if (IS_OUT_OF_CAM_RANGE_TYPED(u16, screenX, screenY)) {
+        ia->x = toggle->base.spriteX;
+        TaskDestroy(gCurTask);
+    } else {
+        if (!(gPlayer.moveState & (MOVESTATE_400000 | MOVESTATE_DEAD))
+            && posX <= Q_24_8_TO_INT(gPlayer.x)) {
+            // __0806332C
+            gPlayer.checkPointX = gUnknown_080D63FC[gCurrentLevel][0];
+            gPlayer.checkPointY = gUnknown_080D63FC[gCurrentLevel][1];
+            gPlayer.checkpointTime = gUnknown_030053E4;
+
+            if (gUnknown_030055B0 == 0)
+                gUnknown_030055B0++;
+
+            TaskDestroy(gCurTask);
+        } else {
+            // _08063388
+            if (gUnknown_030055B0 != 0)
+                TaskDestroy(gCurTask);
+        }
+    }
+}
 
 void initSprite_Interactable_Toggle_Checkpoint(Interactable *in_ia, u16 spriteRegionX,
                                                u16 spriteRegionY, u8 spriteY)
