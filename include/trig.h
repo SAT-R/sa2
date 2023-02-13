eb770338bb2ce89a34f84c2cee3ff9d9c9e1749f
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-02-13 12:23:59 +0100

    Add SIN_24_8 and COS_24_8

--- include/trig.h
+++ include/trig.h
@@ -10,6 +10,9 @@ extern const s16 gSineTable[1280];
 #define SIN(radAngle) (gSineTable[(radAngle)])
 #define COS(radAngle) (gSineTable[(radAngle) + 256])
 
+#define SIN_24_8(radAngle) Q_2_14_TO_Q_24_8(SIN(radAngle))
+#define COS_24_8(radAngle) Q_2_14_TO_Q_24_8(COS(radAngle))
+
 #define DEG_TO_SIN(deg) ((int)((((double)(deg)) / 360.0) * (ONE_CYCLE + 1)))
 
 // NOTE: Returns a Q_24_8!
