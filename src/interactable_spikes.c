f3f8c5839e5ca6c383a24b41272b981ad10b133a
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-02-20 21:46:59 +0100

    Match sub_805FBA0

--- src/interactable_spikes.c
+++ src/interactable_spikes.c
@@ -1,5 +1,6 @@
 #include "global.h"
 #include "task.h"
+#include "m4a.h"
 #include "malloc_vram.h"
 
 #include "interactable.h"
@@ -33,6 +34,11 @@ extern void sub_805FBA0(void);
 extern bool32 sub_80601F8(Sprite *, Interactable *, Sprite_Spikes *, Player *);
 extern bool32 sub_8060440(Sprite *, Interactable *, Sprite_Spikes *, Player *);
 extern void TaskDestructor_8060CF4(struct Task *);
+
+// TODO: Include header this belongs to
+extern u32 sub_800CCB8(Sprite *, s32 x, s32 y, Player *);
+
+extern u32 sub_8060D08(Sprite *, s32 x, s32 y, Player *);
 
 void initSprite_Interactable_Spikes_Up(Interactable *ia, u16 spriteRegionX,
                                        u16 spriteRegionY, u8 spriteY)
@@ -250,4 +256,130 @@ void initSprite_Interactable_Spikes_LeftRight(Interactable *ia, u16 spriteRegion
     }
 
     sub_8004558(s);
+}
+
+void sub_805FBA0(void)
+{
+    // Decls had to be split to match, for some reason
+    s16 screenX, screenY;
+    Sprite *s;
+    Interactable *ia;
+    Sprite_Spikes *spikes;
+    spikes = TaskGetStructPtr(gCurTask);
+    s = &spikes->s;
+    ia = spikes->base.ia;
+
+    screenX = SpriteGetScreenPos(spikes->base.spriteX, spikes->base.regionX);
+    screenY = SpriteGetScreenPos(ia->y, spikes->base.regionY);
+
+    s->x = screenX - gCamera.x;
+    s->y = screenY - gCamera.y;
+
+    if (gGameMode != GAME_MODE_MULTI_PLAYER_COLLECT_RINGS || ia->d.sData[0] != 0
+        || gUnknown_030053E0 != 0) {
+        // _0805FC16
+        s32 r4 = sub_800CCB8(s, screenX, screenY, &gPlayer);
+#ifdef NON_MATCHING
+        u32 gravityInverted;
+#else
+        register u32 gravityInverted asm("r9");
+#endif
+        gravityInverted = gUnknown_03005424 & EXTRA_STATE__GRAVITY_INVERTED;
+        if (gravityInverted) {
+            if (r4 & 0x10000) {
+                gPlayer.y += (r4 << 24) >> 16;
+                gPlayer.speedAirY = 0;
+                gPlayer.moveState &= (~MOVESTATE_8);
+                gPlayer.moveState |= MOVESTATE_IN_AIR;
+                gPlayer.unk3C = 0;
+            }
+            // _0805FC6C
+
+            if (r4 & 0x20000) {
+                if (sub_8060D08(s, screenX, screenY, &gPlayer) & 0x10000) {
+                    gPlayer.y += (r4 << 24) >> 16;
+                    gPlayer.speedAirY = 0;
+                    gPlayer.moveState |= MOVESTATE_8;
+                    gPlayer.moveState &= ~MOVESTATE_IN_AIR;
+                    gPlayer.unk3C = s;
+                }
+            }
+        } else {
+            // _0805FCC8
+            if (r4 & 0x10000) {
+                if (sub_8060D08(s, screenX, screenY, &gPlayer) & 0x10000) {
+                    gPlayer.y += (r4 << 24) >> 16;
+                    gPlayer.speedAirY = gravityInverted; // Q_8_8(0.5) instead?
+                }
+            }
+
+            if (r4 & 0x20000) {
+                gPlayer.y += (r4 << 24) >> 16;
+                gPlayer.speedAirY = Q_8_8(0);
+            }
+        }
+
+        // _0805FD12
+        if (r4 & 0x40000) {
+#ifdef NON_MATCHING
+            u16 iaIndex;
+#else
+            register u16 iaIndex asm("r0");
+#endif
+            gPlayer.moveState |= MOVESTATE_20;
+            gPlayer.x += (s16)((u32)r4 & 0xFF00);
+            gPlayer.speedAirX = 0;
+            gPlayer.speedGroundX = 0;
+
+            iaIndex = IA__SPIKES__NORMAL_LEFT;
+            if (iaIndex != ia->index) {
+                if (sub_800CBA4(&gPlayer)) {
+                    m4aSongNumStart(SE_SPIKES);
+                }
+            }
+        } else {
+            // _0805FD60
+            if (r4 & 0x80000) {
+#ifdef NON_MATCHING
+                u16 iaIndex;
+#else
+                register u16 iaIndex asm("r0");
+#endif
+                gPlayer.moveState |= MOVESTATE_20;
+                gPlayer.x += (s16)((u32)r4 & 0xFF00);
+                gPlayer.speedAirX = 0;
+                gPlayer.speedGroundX = 0;
+
+                iaIndex = IA__SPIKES__NORMAL_LEFT;
+                if (iaIndex == ia->index) {
+                    if (sub_800CBA4(&gPlayer)) {
+                        m4aSongNumStart(SE_SPIKES);
+                    }
+                }
+            }
+        }
+    }
+    // _0805FDA4
+
+    if ((gGameMode == GAME_MODE_MULTI_PLAYER_COLLECT_RINGS) && (ia->d.sData[0] == 0)
+        && (gUnknown_030053E0 == 0)) {
+        if (spikes->unk3C & 0x20) {
+            gPlayer.moveState &= ~MOVESTATE_20;
+        }
+        // _0805FDD8
+        if (spikes->unk3C & 0x8) {
+            gPlayer.moveState &= ~MOVESTATE_8;
+        }
+    }
+    // _0805FDF0
+    if (IS_OUT_OF_RANGE_OLD(u16, s->x, s->y, (CAM_REGION_WIDTH))) {
+        ia->x = spikes->base.spriteX;
+        TaskDestroy(gCurTask);
+    } else {
+        if ((gGameMode != GAME_MODE_MULTI_PLAYER_COLLECT_RINGS) || (ia->d.sData[0] != 0)
+            || (gUnknown_030053E0 != 0)) {
+            sub_8004558(s);
+            sub_80051E8(s);
+        }
+    }
 }
