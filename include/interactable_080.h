bc99aedb9b9a505ba68d1b696e5368818c37fdb1
Oliver Bell <freshollie@gmail.com>
2023-02-20 00:19:07 +0000

    start on ia80

---
+++ include/interactable_080.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_INTERACTABLE_080_H
+#define GUARD_INTERACTABLE_080_H
+
+#include "global.h"
+#include "interactable.h"
+
+void initSprite_Interactable080(Interactable *ia, u16 spriteRegionX, u16 spriteRegionY,
+                                u8 spriteY);
+
+#endif
