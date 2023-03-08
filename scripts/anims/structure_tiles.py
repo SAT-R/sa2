7c27d0f0976d850b5de5fee96e43d30a99929957
Oliver Bell <freshollie@gmail.com>
2023-03-08 12:44:32 +0000

    restructure anims folder again

--- scripts/anims/structure_tiles.py
+++ scripts/anims/structure_tiles.py
@@ -1,14 +1,14 @@
 import os
 
-for file in os.listdir("graphics/obj_tiles"):
+for file in os.listdir("graphics/obj_tiles/4bpp"):
     if file.endswith(".png"):
         anim_segment = file.split("_")[0]
         id = int(anim_segment.split("a")[1])
         
         folder = f"graphics/obj_tiles/4bpp/anim_{id:04}"
-        
+
         os.makedirs(folder, exist_ok=True)
         
-        os.rename(f"graphics/obj_tiles/{file}", f"{folder}/{file.replace(anim_segment + '_', '')}")
-        print(f"graphics/obj_tiles/{file}", f"{folder}/{file.replace(anim_segment + '_', '')}")
+        os.rename(f"graphics/obj_tiles/4bpp/{file}", f"{folder}/{file.replace(anim_segment + '_', '')}")
+        print(f"graphics/obj_tiles/4bpp/{file}", f"{folder}/{file.replace(anim_segment + '_', '')}")
 
