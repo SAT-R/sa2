30d8f7f26c4b4717d7710e63d2370b78e5d83eea
Oliver Bell <freshollie@gmail.com>
2023-03-12 20:27:52 +0000

    decompile ia69

---
+++ include/game/interactables_2/ice_paradise/069.h
@@ -0,0 +1,9 @@
+#ifndef GUARD_INTERACTABLE_069_H
+#define GUARD_INTERACTABLE_069_H
+
+#include "global.h"
+#include "game/entity.h"
+
+void initSprite_Interactable069(MapEntity *, u16, u16, u8);
+
+#endif
