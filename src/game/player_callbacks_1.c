148982c56207ae7d3cf782ed9d66672eee849c86
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-03-14 16:56:31 +0100

    Match PlayerCB_8025548

--- src/game/player_callbacks_1.c
+++ src/game/player_callbacks_1.c
@@ -24,6 +24,11 @@ void sub_802A360(Player *);
  *       if they are only called directly in the code, those are NOT callbacks.
  */
 
+// >> acceleration = (sin(angle) * 3) / 32
+#define GET_ROTATED_ACCEL(angle) ((SIN_24_8((angle)*4) * 3) >> 5)
+
+#define PLAYER_AIR_SPEED_MAX Q_24_8(15.0)
+
 void PlayerCB_8025318(Player *player)
 {
     u32 mask;
@@ -69,12 +74,10 @@ void PlayerCB_Idle(Player *player)
         sub_802A360(player);
     } else if (!sub_802A0C8(player) && !sub_802A0FC(player) && !sub_8029E6C(player)
                && !sub_802A2A8(player)) {
-        // _0802542C
         sub_802966C(player);
 
         if (((player->unk24 + Q_24_8(0.375)) & 0xFF) < 0xC0) {
-            // >> velocity = (sin(angle) * 3) / 32
-            s32 acceleration = (SIN_24_8(player->unk24 * 4) * 3) >> 5;
+            s32 acceleration = GET_ROTATED_ACCEL(player->unk24);
 
             if (player->speedGroundX != 0) {
                 player->speedGroundX += acceleration;
@@ -90,7 +93,7 @@ void PlayerCB_Idle(Player *player)
             player->speedAirY = -player->speedAirY;
         }
 
-        player->speedAirY = MIN(player->speedAirY, Q_24_8(15.0));
+        player->speedAirY = MIN(player->speedAirY, PLAYER_AIR_SPEED_MAX);
 
         player->y = (gUnknown_03005424 & EXTRA_STATE__GRAVITY_INVERTED)
             ? player->y - player->speedAirY
@@ -109,11 +112,62 @@ void PlayerCB_Idle(Player *player)
                 player->unk2A = GBA_FRAMES_PER_SECOND / 2;
             }
         }
-        // _08025510
 
         if (player->moveState & MOVESTATE_8000) {
             player->moveState &= ~MOVESTATE_IN_AIR;
         } else if (player->moveState & MOVESTATE_IN_AIR) {
+            gPlayer.callback = PlayerCB_8025E18;
+        }
+    }
+}
+
+void PlayerCB_8025548(Player *player)
+{
+    if (!sub_802A0FC(player) && !sub_8029E6C(player)
+        && ((gGameMode == GAME_MODE_MULTI_PLAYER_COLLECT_RINGS)
+            || !sub_802A2A8(player))) {
+        if (player->unk90->unk1C & 0x4000) {
+            gPlayer.callback = PlayerCB_8025318;
+        }
+
+        if (((player->unk24 + Q_24_8(0.375)) & 0xFF) < 0xC0) {
+            u32 acceleration = GET_ROTATED_ACCEL(player->unk24);
+
+            if (player->speedGroundX != 0)
+                player->speedGroundX += acceleration;
+        }
+        // _080255C2
+
+        sub_80232D0(player);
+        sub_8023260(player);
+
+        player->x += player->speedAirX;
+
+        if ((gUnknown_03005424 ^ gUnknown_0300544C) & EXTRA_STATE__GRAVITY_INVERTED) {
+            player->speedAirY = -player->speedAirY;
+        }
+
+        player->speedAirY = MIN(player->speedAirY, PLAYER_AIR_SPEED_MAX);
+
+        player->y = (gUnknown_03005424 & EXTRA_STATE__GRAVITY_INVERTED)
+            ? player->y - player->speedAirY
+            : player->y + player->speedAirY;
+
+        sub_8022D6C(player);
+
+        if (player->unk2A) {
+            player->unk2A -= 1;
+        } else if ((player->unk24 + 32) & 0xC0) {
+            s32 absGroundSpeed = ABS(player->speedGroundX);
+            if (absGroundSpeed < Q_24_8(1.875)) {
+                player->speedGroundX = 0;
+
+                player->moveState |= MOVESTATE_IN_AIR;
+                player->unk2A = GBA_FRAMES_PER_SECOND / 2;
+            }
+        }
+
+        if (player->moveState & MOVESTATE_IN_AIR) {
             gPlayer.callback = PlayerCB_8025E18;
         }
     }
