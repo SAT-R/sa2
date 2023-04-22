ad5360f97e9130a49d8d790fbb76f1e47086a706
Oliver Bell <freshollie@gmail.com>
2023-04-22 15:56:14 +0100

    finish decompiling flying handle

--- src/game/interactables_2/sky_canyon/flying_handle.c
+++ src/game/interactables_2/sky_canyon/flying_handle.c
@@ -4,33 +4,51 @@
 #include "game/interactables_2/sky_canyon/flying_handle.h"
 #include "malloc_vram.h"
 #include "trig.h"
+#include "lib/m4a.h"
 
 #include "constants/move_states.h"
 #include "constants/animations.h"
+#include "constants/songs.h"
 
 typedef struct {
     SpriteBase base;
     Sprite sprite;
-    s32 unk3C;
-    s32 unk40;
+    s32 x;
+    s32 y;
     s32 unk44;
     s32 unk48;
     s32 unk4C;
     s32 unk50;
-    s16 unk54;
-    s16 unk56;
-    s16 unk58;
-    s16 unk5A;
+    s16 offsetX;
+    s16 offsetY;
+    s16 width;
+    s16 height;
     s16 unk5C;
     s16 unk5E;
     s32 unk60;
-    s32 filler64;
-    s8 unk68;
+    u32 unk64;
+    u8 unk68;
+    u8 unk69;
     s32 unk6C[3][2];
-} Sprite_FlyingHandle; /* 0x84 */
+} Sprite_FlyingHandle;
 
-void Task_Interactable_SkyCanyon_FlyingHandle(void);
-void TaskDestructor_Interactable_SkyCanyon_FlyingHandle(struct Task *);
+static void Task_Interactable_SkyCanyon_FlyingHandle(void);
+static void TaskDestructor_Interactable_SkyCanyon_FlyingHandle(struct Task *);
+static void sub_807F8B4(Sprite_FlyingHandle *);
+static void sub_807F484(Sprite_FlyingHandle *);
+static void sub_807F798(Sprite_FlyingHandle *);
+static void sub_807F818(Sprite_FlyingHandle *);
+static void sub_807F770(Sprite_FlyingHandle *);
+static void sub_807F4F0(Sprite_FlyingHandle *);
+static void sub_807F7D0(Sprite_FlyingHandle *);
+static void sub_807F5C0(Sprite_FlyingHandle *);
+static void Task_ActiveMain(void);
+static void sub_807F6F0(void);
+static bool32 ShouldDespawn(Sprite_FlyingHandle *);
+static void Despawn(Sprite_FlyingHandle *);
+static void sub_807F784(Sprite_FlyingHandle *);
+
+#define FLYING_HANDLE_VRAM_TILES 25
 
 void initSprite_Interactable_SkyCanyon_FlyingHandle(MapEntity *me, u16 spriteRegionX,
                                                     u16 spriteRegionY, u8 spriteY)
@@ -45,16 +63,16 @@ void initSprite_Interactable_SkyCanyon_FlyingHandle(MapEntity *me, u16 spriteReg
     flyingHandle->unk5C = 0;
     flyingHandle->unk5E = 0;
     flyingHandle->unk60 = 0;
-    flyingHandle->unk3C = SpriteGetScreenPos(me->x, spriteRegionX);
-    flyingHandle->unk40 = SpriteGetScreenPos(me->y, spriteRegionY);
-    flyingHandle->unk54 = me->d.sData[0] * 8;
-    flyingHandle->unk56 = me->d.sData[1] * 8;
-    flyingHandle->unk58 = flyingHandle->unk54 + me->d.uData[2] * 8;
-    flyingHandle->unk5A = flyingHandle->unk56 + me->d.uData[3] * 8;
+    flyingHandle->x = SpriteGetScreenPos(me->x, spriteRegionX);
+    flyingHandle->y = SpriteGetScreenPos(me->y, spriteRegionY);
+    flyingHandle->offsetX = me->d.sData[0] * 8;
+    flyingHandle->offsetY = me->d.sData[1] * 8;
+    flyingHandle->width = flyingHandle->offsetX + me->d.uData[2] * 8;
+    flyingHandle->height = flyingHandle->offsetY + me->d.uData[3] * 8;
 
-    flyingHandle->unk44 = Q_24_8(((flyingHandle->unk54 + flyingHandle->unk58) >> 1)
-                                 + flyingHandle->unk3C);
-    flyingHandle->unk48 = Q_24_8(flyingHandle->unk40 + flyingHandle->unk5A);
+    flyingHandle->unk44
+        = Q_24_8(((flyingHandle->offsetX + flyingHandle->width) >> 1) + flyingHandle->x);
+    flyingHandle->unk48 = Q_24_8(flyingHandle->y + flyingHandle->height);
     flyingHandle->unk4C = 0;
     flyingHandle->unk50 = 0;
 
@@ -78,10 +96,257 @@ void initSprite_Interactable_SkyCanyon_FlyingHandle(MapEntity *me, u16 spriteReg
     sprite->unk22 = 0x10;
     sprite->focused = 0;
     sprite->unk28[0].unk0 = -1;
-    sprite->unk10 = 0x2000;
-    sprite->graphics.dest = VramMalloc(0x19);
+    sprite->unk10 = SPRITE_FLAG_MASK_13;
+    sprite->graphics.dest = VramMalloc(FLYING_HANDLE_VRAM_TILES);
     sprite->graphics.anim = SA2_ANIM_FLYING_HANDLE;
     sprite->variant = 0;
     sub_8004558(sprite);
     SET_MAP_ENTITY_INITIALIZED(me);
+}
+
+static void Task_ActiveMain(void)
+{
+    Sprite_FlyingHandle *flyingHandle = TaskGetStructPtr(gCurTask);
+    if (!GAME_MODE_IS_SINGLE_PLAYER(gGameMode)) {
+        sub_807F8B4(flyingHandle);
+    }
+
+    sub_807F818(flyingHandle);
+
+    if (!PlayerIsAlive) {
+        sub_807F770(flyingHandle);
+    } else if (gPlayer.unk2C == 120) {
+        sub_807F798(flyingHandle);
+    } else if (flyingHandle->unk69 == 0) {
+        if (gPlayer.unk5E & gPlayerControls.jump) {
+            if (gPlayer.unk5C & 0x20) {
+                flyingHandle->unk64 = 1;
+            } else {
+                flyingHandle->unk64 = 0;
+            }
+            sub_807F484(flyingHandle);
+        }
+    }
+
+    if (flyingHandle->unk69 != 0) {
+        flyingHandle->unk69--;
+    }
+
+    sub_807F4F0(flyingHandle);
+    sub_807F7D0(flyingHandle);
+    sub_807F5C0(flyingHandle);
+}
+
+static void ActivatePlayerGrab(Sprite_FlyingHandle *flyingHandle)
+{
+    gPlayer.moveState |= MOVESTATE_400000;
+    gPlayer.unk64 = 55;
+    gPlayer.speedGroundX = 0;
+    gPlayer.speedAirX = 0;
+    gPlayer.speedAirY = 0;
+    sub_807F7D0(flyingHandle);
+
+    flyingHandle->unk60 = 1;
+    flyingHandle->unk5E = -384;
+    flyingHandle->unk44 += flyingHandle->unk4C;
+    flyingHandle->unk48 += flyingHandle->unk50;
+    flyingHandle->unk4C = 0;
+    flyingHandle->unk50 = 0;
+    flyingHandle->unk69 = 30;
+    m4aSongNumStart(SE_291);
+    gCurTask->main = Task_ActiveMain;
+}
+
+static void sub_807F484(Sprite_FlyingHandle *flyingHandle)
+{
+    gPlayer.moveState &= ~MOVESTATE_400000;
+    sub_80218E4(&gPlayer);
+    sub_8023B5C(&gPlayer, 14);
+
+    gPlayer.unk16 = 6;
+    gPlayer.unk17 = 14;
+    gPlayer.moveState &= ~MOVESTATE_4;
+    gPlayer.unk6D = 3;
+    gPlayer.speedGroundX = 0;
+    gPlayer.speedAirX = 0;
+    gPlayer.speedAirY = 0;
+    flyingHandle->unk69 = 30;
+    m4aSongNumStop(SE_291);
+    gCurTask->main = sub_807F6F0;
+}
+
+static void sub_807F4F0(Sprite_FlyingHandle *flyingHandle)
+{
+    if (flyingHandle->unk60 != 0) {
+        if (flyingHandle->unk48 <= Q_24_8(flyingHandle->y + flyingHandle->offsetY)
+            && flyingHandle->unk5E > -1) {
+            flyingHandle->unk50 = SIN_24_8(flyingHandle->unk68 * 4) * 8;
+            flyingHandle->unk68 += 4;
+            flyingHandle->unk5E = 0;
+        } else {
+            flyingHandle->unk5E += 16;
+            flyingHandle->unk5E = MIN(flyingHandle->unk5E, 0x300);
+            flyingHandle->unk48 -= flyingHandle->unk5E;
+            if (flyingHandle->unk48 <= Q_24_8(flyingHandle->y + flyingHandle->offsetY)) {
+                flyingHandle->unk5E = 0;
+                flyingHandle->unk48 = Q_24_8(flyingHandle->y + flyingHandle->offsetY);
+                flyingHandle->unk68 = -128;
+            }
+        }
+    } else {
+        flyingHandle->unk48 = Q_24_8(flyingHandle->y + flyingHandle->height);
+        flyingHandle->unk50 = SIN_24_8(flyingHandle->unk68 * 4) * 8;
+        flyingHandle->unk68 += 4;
+    }
+}
+
+static void sub_807F5C0(Sprite_FlyingHandle *flyingHandle)
+{
+    Sprite *sprite = &flyingHandle->sprite;
+    if (!GAME_MODE_IS_SINGLE_PLAYER(gGameMode)) {
+        sprite->x = Q_24_8_TO_INT(flyingHandle->unk6C[1][0]) - gCamera.x;
+        sprite->y = Q_24_8_TO_INT(flyingHandle->unk6C[1][1]) - gCamera.y;
+    } else {
+        sprite->x = Q_24_8_TO_INT(flyingHandle->unk44 + flyingHandle->unk4C) - gCamera.x;
+        sprite->y = Q_24_8_TO_INT(flyingHandle->unk48 + flyingHandle->unk50) - gCamera.y;
+    }
+
+    sub_8004558(sprite);
+    sub_80051E8(sprite);
+}
+
+static bool32 IsPlayerTouching(Sprite_FlyingHandle *flyingHandle)
+{
+    if (!PlayerIsAlive) {
+        return FALSE;
+    } else {
+        s16 x = Q_24_8_TO_INT(flyingHandle->unk44 + flyingHandle->unk4C) - gCamera.x;
+        s16 y = Q_24_8_TO_INT(flyingHandle->unk48 + flyingHandle->unk50) - gCamera.y;
+        s16 playerX = (Q_24_8_TO_INT(gPlayer.x) - gCamera.x);
+        s16 playerY = (Q_24_8_TO_INT(gPlayer.y) - gCamera.y);
+        s16 dX = x - playerX;
+        s16 dY = y - playerY;
+        if (dX * dX + dY * dY <= (16 * 16)) {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+static void Task_Interactable_SkyCanyon_FlyingHandle(void)
+{
+    Sprite_FlyingHandle *flyingHandle = TaskGetStructPtr(gCurTask);
+    if (!GAME_MODE_IS_SINGLE_PLAYER(gGameMode)) {
+        sub_807F8B4(flyingHandle);
+    }
+
+    if (IsPlayerTouching(flyingHandle)) {
+        ActivatePlayerGrab(flyingHandle);
+    }
+
+    if (ShouldDespawn(flyingHandle)) {
+        Despawn(flyingHandle);
+    } else {
+        sub_807F4F0(flyingHandle);
+        sub_807F5C0(flyingHandle);
+    }
+}
+
+static void sub_807F6F0(void)
+{
+    Sprite_FlyingHandle *flyingHandle = TaskGetStructPtr(gCurTask);
+
+    if (!GAME_MODE_IS_SINGLE_PLAYER(gGameMode)) {
+        sub_807F8B4(flyingHandle);
+    }
+
+    if (flyingHandle->unk69 == 0) {
+        if (!PlayerIsAlive || IsPlayerTouching(flyingHandle) == 0) {
+            sub_807F784(flyingHandle);
+        }
+    } else {
+        flyingHandle->unk69 -= 1;
+    }
+
+    sub_807F4F0(flyingHandle);
+    sub_807F5C0(flyingHandle);
+}
+
+static void TaskDestructor_Interactable_SkyCanyon_FlyingHandle(struct Task *t)
+{
+    Sprite_FlyingHandle *flyingHandle = TaskGetStructPtr(t);
+    VramFree(flyingHandle->sprite.graphics.dest);
+}
+
+static void sub_807F770(UNUSED Sprite_FlyingHandle *flyingHandle)
+{
+    gCurTask->main = Task_Interactable_SkyCanyon_FlyingHandle;
+}
+
+static void sub_807F784(UNUSED Sprite_FlyingHandle *flyingHandle)
+{
+    gCurTask->main = Task_Interactable_SkyCanyon_FlyingHandle;
+}
+
+static void sub_807F798(Sprite_FlyingHandle *flyingHandle)
+{
+    gPlayer.moveState &= ~MOVESTATE_400000;
+    flyingHandle->unk69 = 30;
+    m4aSongNumStop(SE_291);
+    gCurTask->main = sub_807F6F0;
+}
+
+static void sub_807F7D0(Sprite_FlyingHandle *flyingHandle)
+{
+#ifndef NON_MATCHING
+    register Player *p1 asm("r0") = &gPlayer;
+    register u32 r1 asm("r1") = p1->moveState & MOVESTATE_FACING_LEFT;
+    register Player *player asm("r2") = p1;
+#else
+    Player *player = &gPlayer;
+    u32 r1 = p1->moveState & MOVESTATE_FACING_LEFT;
+#endif
+    if (r1) {
+        player->x = (flyingHandle->unk44 + flyingHandle->unk4C) + Q_24_8(4.0);
+        player->y = (flyingHandle->unk48 + flyingHandle->unk50) + Q_24_8(20.0);
+    } else {
+        player->x = (flyingHandle->unk44 + flyingHandle->unk4C) - Q_24_8(4.0);
+        player->y = (flyingHandle->unk48 + flyingHandle->unk50) + Q_24_8(20.0);
+    }
+}
+
+static void sub_807F818(UNUSED Sprite_FlyingHandle *flyingHandle)
+{
+    m4aSongNumStartOrContinue(SE_291);
+}
+
+static bool32 ShouldDespawn(Sprite_FlyingHandle *flyingHandle)
+{
+    s16 x = flyingHandle->x - gCamera.x;
+    s16 y = flyingHandle->y - gCamera.y;
+
+    if (x + flyingHandle->width < -128 || x + flyingHandle->offsetX > 368
+        || y + flyingHandle->height < -128 || y + flyingHandle->offsetY > 288) {
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+static void Despawn(Sprite_FlyingHandle *flyingHandle)
+{
+    SET_MAP_ENTITY_NOT_INITIALIZED(flyingHandle->base.me, flyingHandle->base.spriteX);
+    TaskDestroy(gCurTask);
+}
+
+static void sub_807F8B4(Sprite_FlyingHandle *flyingHandle)
+{
+    flyingHandle->unk6C[2][0] = flyingHandle->unk6C[1][0];
+    flyingHandle->unk6C[2][1] = flyingHandle->unk6C[1][1];
+
+    flyingHandle->unk6C[1][0] = flyingHandle->unk6C[0][0];
+    flyingHandle->unk6C[1][1] = flyingHandle->unk6C[0][1];
+
+    flyingHandle->unk6C[0][0] = flyingHandle->unk44 + flyingHandle->unk4C;
+    flyingHandle->unk6C[0][1] = flyingHandle->unk48 + flyingHandle->unk50;
 }
