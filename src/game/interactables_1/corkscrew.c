5dc15ad2e724a1968ad3f32b54cdee47ff0f2fe4
Oliver Bell <freshollie@gmail.com>
2023-04-25 01:03:30 +0100

    more corkscrew

--- src/game/interactables_1/corkscrew.c
+++ src/game/interactables_1/corkscrew.c
@@ -11,6 +11,7 @@
 
 #include "constants/animations.h"
 #include "constants/move_states.h"
+#include "constants/songs.h"
 
 typedef struct {
     SpriteBase base;
@@ -39,18 +40,18 @@ void Task_8060D34(void)
                     >= Q_24_8_TO_INT(gPlayer.y))) {
             if (x < Q_24_8_TO_INT(gPlayer.x) && (corkscrew->base.spriteY & 1)) {
                 s32 idx;
-                s32 yVal;
+                s32 y24_8;
                 gPlayer.unk6D = 27;
 
                 idx = ((((Q_24_8_TO_INT(gPlayer.x) - x) * 930) >> 8) + 256) & ONE_CYCLE;
                 gPlayer.x += gPlayer.speedGroundX;
-                yVal = (y * 0x100);
-                gPlayer.y = SIN_24_8(idx) * 0x18 + yVal - 7168;
+                y24_8 = Q_24_8(y);
+                gPlayer.y = SIN_24_8(idx) * 24 + y24_8 - Q_24_8(28);
 
                 gPlayer.speedAirY = 0;
                 gCurTask->main = sub_8060ED0;
-            } else if ((x >= Q_24_8_TO_INT(gPlayer.x)) && gPlayer.speedGroundX > 0x400
-                       && !(gPlayer.moveState & 2)
+            } else if ((x >= Q_24_8_TO_INT(gPlayer.x)) && gPlayer.speedGroundX > Q_8_8(4)
+                       && !(gPlayer.moveState & MOVESTATE_IN_AIR)
                        && !(gPlayer.unk5E & gPlayerControls.jump)) {
                 corkscrew->base.spriteY |= 1;
             } else {
@@ -67,5 +68,66 @@ void Task_8060D34(void)
     if (IS_OUT_OF_CAM_RANGE_TYPED(u32, x, y)) {
         SET_MAP_ENTITY_NOT_INITIALIZED(me, corkscrew->base.spriteX);
         TaskDestroy(gCurTask);
+    }
+}
+
+void sub_8060ED0(void)
+{
+    s32 y24_8, idx;
+
+    Sprite_Corkscrew *corkscrew = TaskGetStructPtr(gCurTask);
+    MapEntity *me = corkscrew->base.me;
+
+    u16 regionX = corkscrew->base.regionX;
+    u16 regionY = corkscrew->base.regionY;
+    Player *player = &gPlayer;
+    s32 x = TO_WORLD_POS(corkscrew->base.spriteX, regionX);
+    s32 y = TO_WORLD_POS(me->y, regionY);
+
+    if (!PLAYER_IS_ALIVE) {
+        SET_MAP_ENTITY_NOT_INITIALIZED(me, corkscrew->base.spriteX);
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    if (Q_24_8_TO_INT(player->x) - x > 0x230) {
+        if (player->moveState & MOVESTATE_4) {
+            player->unk6D = 2;
+        } else {
+            player->unk6D = 1;
+        }
+        gCurTask->main = Task_8060D34;
+        return;
+    }
+
+    idx = Q_24_8_TO_INT((Q_24_8_TO_INT(player->x) - x) * 930);
+    idx = (idx + 256) & ONE_CYCLE;
+
+    player->x += player->speedGroundX;
+    y24_8 = Q_24_8(y);
+    player->y = SIN_24_8(idx) * 24 + y24_8 - Q_24_8(28);
+    player->speedAirY = 0;
+
+    if (player->speedGroundX < corkscrew->unk10) {
+        player->unk64 = 50;
+        player->speedAirX = player->speedGroundX;
+        player->unk6D = 5;
+        gCurTask->main = Task_8060D34;
+    } else if (player->unk5E & gPlayerControls.jump) {
+        player->unk64 = 50;
+        player->speedAirX = player->speedGroundX;
+        player->speedAirY = -Q_24_8(4.875);
+        player->unk6D = 5;
+        gCurTask->main = Task_8060D34;
+    } else if (!(player->moveState & MOVESTATE_4) && player->unk5E & DPAD_DOWN) {
+        player->unk64 = 4;
+        sub_8023B5C(player, 9);
+        player->unk16 = 6;
+        player->unk17 = 9;
+        player->moveState |= MOVESTATE_4;
+        m4aSongNumStart(SE_SPIN_ATTACK);
+    } else if (!(player->moveState & MOVESTATE_4)) {
+        player->unk68 = PlayerCharacterIdleAnims[player->character] + 68;
+        player->unk6A = Div((idx - 282) & ONE_CYCLE, 94);
     }
 }
