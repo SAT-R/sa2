b586fe543aeac21bdb1f9842a3f36d911ab69128
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-05-28 15:40:21 +0200

    Format

--- include/animation_commands_extra.h
+++ include/animation_commands_extra.h
@@ -3,7 +3,7 @@
 
 #include "gba/types.h"
 
-s32 sub_8004274(void *dest, const void *tilesSrc, u16 param2,
-                u16 param3, u8 bgCtrlIndex, const u8 *tileOffsets, u8 param6);
+s32 sub_8004274(void *dest, const void *tilesSrc, u16 param2, u16 param3, u8 bgCtrlIndex,
+                const u8 *tileOffsets, u8 param6);
 
 #endif // GUARD_ANIMATION_COMMANDS_EXTRA_H
