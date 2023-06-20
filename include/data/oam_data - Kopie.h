a323a7619c660bad3cb27d6bcd6b173419b26628
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-06-21 01:26:51 +0200

    Remove redundant copy

--- include/data/oam_data - Kopie.h
+++
@@ -1,8 +0,0 @@
-#ifndef GUARD_CONST_DATA_SPRITE_DIMENSIONS_H
-#define GUARD_CONST_DATA_SPRITE_DIMENSIONS_H
-
-#include "gba/types.h"
-
-extern const SpriteOffset *const gSpriteDimensions[1133];
-
-#endif GUARD_CONST_DATA_SPRITE_DIMENSIONS_H
