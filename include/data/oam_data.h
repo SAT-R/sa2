c6c808956f42b7d443e963a582ab065099ebe1f6
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-06-21 22:44:01 +0200

    Fix formatter's formatting

--- include/data/oam_data.h
+++ include/data/oam_data.h
@@ -10,18 +10,23 @@
 //       usually generated during runtime, anyway.
 //       That's what this variation of 'OamData' is for.
 struct PACKED OamDataRom {
-    /*0x00*/ u32 y : 8;
-    /*0x01*/ u32 affineMode : 2; // 0x1, 0x2 -> 0x4
+    /*0x00*/
+    u32 y : 8;
+
+    /*0x01*/
+    u32 affineMode : 2; // 0x1, 0x2 -> 0x4
     u32 objMode : 2; // 0x4, 0x8 -> 0xC
     u32 mosaic : 1; // 0x10
     u32 bpp : 1; // 0x20
     u32 shape : 2; // 0x40, 0x80 -> 0xC0
 
-    /*0x02*/ u32 x : 9;
+    /*0x02*/
+    u32 x : 9;
     u32 matrixNum : 5; // bits 3/4 are h-flip/v-flip if not in affine mode
     u32 size : 2; // 0x4000, 0x8000 -> 0xC000
 
-    /*0x04*/ u16 tileNum : 10; // 0x3FF
+    /*0x04*/
+    u16 tileNum : 10; // 0x3FF
     u16 priority : 2; // 0x400, 0x800 -> 0xC00
     u16 paletteNum : 4;
 };
