2e4d6a97b782e7db521c419f6a3829a1d2caa107
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-07-10 13:45:03 +0200

    Consistent naming

--- include/game/game_5.h
+++ include/game/game_5.h
@@ -1,6 +1,6 @@
 #ifndef GUARD_GAME_5_H
 #define GUARD_GAME_5_H
 
-void setPlayerControls(u16 jump, u16 attack, u16 trick);
+void SetPlayerControls(u16 jump, u16 attack, u16 trick);
 
 #endif // GUARD_GAME_5_H
