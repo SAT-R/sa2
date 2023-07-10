b5660db7d6f53e989dc176545b4795f169416076
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-07-10 17:00:45 +0200

    Add note about rodata

--- asm/game.s
+++ asm/game.s
@@ -3,6 +3,9 @@
 
 .section .rodata
 
+@; NOTE / FIXME(?): These are only used by player_super_sonic.
+@;                  But that already includes .rodata, which is 
+@                   away from gUnknown_080D650C & gUnknown_080D661C
     .global gUnknown_080D650C
 gUnknown_080D650C:
     .incbin "baserom.gba", 0x000D650C, 0x110
