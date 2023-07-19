aba43506ce4c83ccc31b861f317f17ae965fe171
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-07-19 16:37:56 +0200

    Match Enemy Circus

--- src/game/enemies/circus.c
+++ src/game/enemies/circus.c
@@ -165,3 +165,49 @@ void Task_8055C0C(void)
         gCurTask->main = Task_8055D7C;
     }
 }
+
+void Task_8055D7C(void)
+{
+    Sprite_Circus *circus = TaskGetStructPtr(gCurTask);
+    Sprite *s = &circus->s;
+    Sprite *s2 = &circus->s2;
+    MapEntity *me = circus->base.me;
+    Vec2_32 pos;
+
+    pos.x = Q_24_8_TO_INT(circus->spawnX);
+    pos.y = Q_24_8_TO_INT(circus->spawnY);
+    s->x = pos.x - gCamera.x;
+    s->y = pos.y - gCamera.y;
+
+    ENEMY_DESTROY_IF_PLAYER_HIT_2(s, pos);
+
+    ENEMY_DESTROY_IF_OUT_OF_CAM_RANGE(circus, me, s);
+
+    sub_80122DC(circus->spawnX, circus->spawnY);
+
+    if (sub_8004558(s) == 0) {
+        sub_80051E8(s);
+
+        circus->unk84 = 30;
+
+        s->graphics.anim = SA2_ANIM_CIRCUS;
+        s->variant = 0;
+        s->unk21 = -1;
+        gCurTask->main = Task_CircusMain;
+    } else {
+        sub_80051E8(s);
+    }
+
+    s2->x = s->x;
+    s2->y = s->y - Q_24_8(0.125);
+
+    sub_800C84C(s2, pos.x, pos.y - 0x20);
+    sub_80051E8(s2);
+}
+
+void TaskDestructor_Circus(struct Task *t)
+{
+    Sprite_Circus *circus = TaskGetStructPtr(t);
+    VramFree(circus->s.graphics.dest);
+    VramFree(circus->s2.graphics.dest);
+}
