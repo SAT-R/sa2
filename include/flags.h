8c8a7e14cb9c17e06881cfe9f930326d621eb4ad
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-07-27 15:03:55 +0200

    Add a few more gFlags placeholders

--- include/flags.h
+++ include/flags.h
@@ -10,15 +10,15 @@
 #define FLAGS_EXECUTE_HBLANK_CALLBACKS   0x8
 #define FLAGS_10                         0x10
 #define FLAGS_20                         0x20
-
-#define FLAGS_NO_FLASH_MEMORY 0x100
-#define FLAGS_200             0x200
-
-#define FLAGS_PAUSE_GAME 0x400
-#define FLAGS_800        0x800
-#define FLAGS_SKIP_INTRO 0x1000
-#define FLAGS_4000       0x4000
-#define FLAGS_8000       0x8000
+#define FLAGS_40                         0x40
+#define FLAGS_80                         0x80
+#define FLAGS_NO_FLASH_MEMORY            0x100
+#define FLAGS_200                        0x200
+#define FLAGS_PAUSE_GAME                 0x400
+#define FLAGS_800                        0x800
+#define FLAGS_SKIP_INTRO                 0x1000
+#define FLAGS_4000                       0x4000
+#define FLAGS_8000                       0x8000
 
 extern u32 gFlags;
 
