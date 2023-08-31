ad6a11e8186d5e1d9147156e25475a77413f691f
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-08-31 23:43:00 +0200

    animcmd_COPY/animCmdTable_2 -> animcmd_BG/animCmdTable_BG

--- src/animation_commands_bg.c
+++ src/animation_commands_bg.c
@@ -11,28 +11,28 @@
 
 #include "animation_commands.h"
 
-static s32 animCmd_GetTiles_COPY(void *, Sprite *);
-static s32 animCmd_GetPalette_COPY(void *, Sprite *);
-static s32 animCmd_JumpBack_COPY(void *, Sprite *);
-static s32 animCmd_End_COPY(void *, Sprite *);
-static s32 animCmd_PlaySoundEffect_COPY(void *, Sprite *);
-static s32 animCmd_AddHitbox_COPY(void *, Sprite *);
-static s32 animCmd_TranslateSprite_COPY(void *, Sprite *);
-static s32 animCmd_8_COPY(void *, Sprite *);
-static s32 animCmd_SetIdAndVariant_COPY(void *, Sprite *);
-static s32 animCmd_10_COPY(void *, Sprite *);
-static s32 animCmd_SetSpritePriority_COPY(void *, Sprite *);
-static s32 animCmd_12_COPY(void *, Sprite *);
+static s32 animCmd_GetTiles_BG(void *, Sprite *);
+static s32 animCmd_GetPalette_BG(void *, Sprite *);
+static s32 animCmd_JumpBack_BG(void *, Sprite *);
+static s32 animCmd_End_BG(void *, Sprite *);
+static s32 animCmd_PlaySoundEffect_BG(void *, Sprite *);
+static s32 animCmd_AddHitbox_BG(void *, Sprite *);
+static s32 animCmd_TranslateSprite_BG(void *, Sprite *);
+static s32 animCmd_8_BG(void *, Sprite *);
+static s32 animCmd_SetIdAndVariant_BG(void *, Sprite *);
+static s32 animCmd_10_BG(void *, Sprite *);
+static s32 animCmd_SetSpritePriority_BG(void *, Sprite *);
+static s32 animCmd_12_BG(void *, Sprite *);
 
 #define ReadInstruction(script, cursor) ((void *)(script) + (cursor * sizeof(s32)))
 
-const AnimationCommandFunc animCmdTable_2[12] = {
-    animCmd_GetTiles_COPY,          animCmd_GetPalette_COPY,
-    animCmd_JumpBack_COPY,          animCmd_End_COPY,
-    animCmd_PlaySoundEffect_COPY,   animCmd_AddHitbox_COPY,
-    animCmd_TranslateSprite_COPY,   animCmd_8_COPY,
-    animCmd_SetIdAndVariant_COPY,   animCmd_10_COPY,
-    animCmd_SetSpritePriority_COPY, animCmd_12_COPY,
+const AnimationCommandFunc animCmdTable_bg[12] = {
+    animCmd_GetTiles_BG,          animCmd_GetPalette_BG,
+    animCmd_JumpBack_BG,          animCmd_End_BG,
+    animCmd_PlaySoundEffect_BG,   animCmd_AddHitbox_BG,
+    animCmd_TranslateSprite_BG,   animCmd_8_BG,
+    animCmd_SetIdAndVariant_BG,   animCmd_10_BG,
+    animCmd_SetSpritePriority_BG, animCmd_12_BG,
 };
 
 void UpdateBgAnimationTiles(Background *bg)
@@ -79,7 +79,7 @@ void UpdateBgAnimationTiles(Background *bg)
 
 // Differences to UpdateSpriteAnimation:
 // - SPRITE_MAYBE_SWITCH_ANIM gets executed *after* the if.
-// - Uses animCmdTable_2 instead of animCmdTable
+// - Uses animCmdTable_bg instead of animCmdTable
 s32 sub_80036E0(Sprite *s)
 {
     ACmd **variants;
@@ -104,7 +104,7 @@ s32 sub_80036E0(Sprite *s)
         script = variants[s->variant];
         cmd = ReadInstruction(script, s->animCursor);
         while (cmd->id < 0) {
-            ret = animCmdTable_2[~cmd->id](cmd, s);
+            ret = animCmdTable_bg[~cmd->id](cmd, s);
             if (ret != 1) {
 #ifndef NON_MATCHING
                 register ACmd *newScript asm("r1");
@@ -147,7 +147,7 @@ s32 sub_80036E0(Sprite *s)
 
 // (-1)
 // No differences to animCmd_GetTiles
-s32 animCmd_GetTiles_COPY(void *cursor, Sprite *s)
+s32 animCmd_GetTiles_BG(void *cursor, Sprite *s)
 {
     ACmd_GetTiles *cmd = (ACmd_GetTiles *)cursor;
     s->animCursor += AnimCommandSizeInWords(ACmd_GetTiles);
@@ -173,8 +173,8 @@ s32 animCmd_GetTiles_COPY(void *cursor, Sprite *s)
 // (-6)
 // Differences to animCmd_AddHitbox:
 // - uses XOR_SWAP macro instead of SWAP_AND_NEGATE
-// TODO: rename animCmd_AddHitbox_COPY
-s32 animCmd_AddHitbox_COPY(void *cursor, Sprite *s)
+// TODO: rename animCmd_AddHitbox_BG
+s32 animCmd_AddHitbox_BG(void *cursor, Sprite *s)
 {
     ACmd_Hitbox *cmd = (ACmd_Hitbox *)cursor;
     s32 index = cmd->hitbox.index & 0xF;
@@ -562,7 +562,7 @@ s32 sub_8004274(void *dest, const void *tilesSrc, u16 param2, u16 param3, u8 bgC
 // - gBgPalette is used instead of gObjPalette
 // - gFlags ->  FLAGS_UPDATE_BACKGROUND_PALETTES
 //   instead of FLAGS_UPDATE_SPRITE_PALETTES
-s32 animCmd_GetPalette_COPY(void *cursor, Sprite *s)
+s32 animCmd_GetPalette_BG(void *cursor, Sprite *s)
 {
     ACmd_GetPalette *cmd = (ACmd_GetPalette *)cursor;
     s->animCursor += AnimCommandSizeInWords(ACmd_GetPalette);
@@ -580,7 +580,7 @@ s32 animCmd_GetPalette_COPY(void *cursor, Sprite *s)
 }
 
 // (-3)
-s32 animCmd_JumpBack_COPY(void *cursor, Sprite *s)
+s32 animCmd_JumpBack_BG(void *cursor, Sprite *s)
 {
     ACmd_JumpBack *cmd = cursor;
     s->animCursor -= cmd->offset;
@@ -589,7 +589,7 @@ s32 animCmd_JumpBack_COPY(void *cursor, Sprite *s)
 }
 
 // (-4)
-s32 animCmd_End_COPY(void *cursor, Sprite *s)
+s32 animCmd_End_BG(void *cursor, Sprite *s)
 {
     SPRITE_FLAG_SET(s, ANIM_OVER);
 
@@ -597,7 +597,7 @@ s32 animCmd_End_COPY(void *cursor, Sprite *s)
 }
 
 // (-5)
-s32 animCmd_PlaySoundEffect_COPY(void *cursor, Sprite *s)
+s32 animCmd_PlaySoundEffect_BG(void *cursor, Sprite *s)
 {
     ACmd_PlaySoundEffect *cmd = cursor;
     s->animCursor += AnimCommandSizeInWords(ACmd_PlaySoundEffect);
@@ -608,7 +608,7 @@ s32 animCmd_PlaySoundEffect_COPY(void *cursor, Sprite *s)
 }
 
 // (-7)
-s32 animCmd_TranslateSprite_COPY(void *cursor, Sprite *s)
+s32 animCmd_TranslateSprite_BG(void *cursor, Sprite *s)
 {
     ACmd_TranslateSprite *cmd = cursor;
     s->animCursor += AnimCommandSizeInWords(ACmd_TranslateSprite);
@@ -620,7 +620,7 @@ s32 animCmd_TranslateSprite_COPY(void *cursor, Sprite *s)
 }
 
 // (-8)
-s32 animCmd_8_COPY(void *cursor, Sprite *s)
+s32 animCmd_8_BG(void *cursor, Sprite *s)
 {
     ACmd_8 *cmd = cursor;
     s->animCursor += AnimCommandSizeInWords(ACmd_8);
@@ -628,7 +628,7 @@ s32 animCmd_8_COPY(void *cursor, Sprite *s)
     return 1;
 }
 
-s32 animCmd_SetIdAndVariant_COPY(void *cursor, Sprite *s)
+s32 animCmd_SetIdAndVariant_BG(void *cursor, Sprite *s)
 {
     ACmd_SetIdAndVariant *cmd = cursor;
     s->animCursor += AnimCommandSizeInWords(ACmd_SetIdAndVariant);
@@ -640,7 +640,7 @@ s32 animCmd_SetIdAndVariant_COPY(void *cursor, Sprite *s)
     return -1;
 }
 
-s32 animCmd_10_COPY(void *cursor, Sprite *s)
+s32 animCmd_10_BG(void *cursor, Sprite *s)
 {
     s->animCursor += AnimCommandSizeInWords(ACmd_10);
 
@@ -652,14 +652,14 @@ s32 animCmd_10_COPY(void *cursor, Sprite *s)
 }
 
 // This is not a NOP-instruction in the regular version
-s32 animCmd_SetSpritePriority_COPY(void *cursor, Sprite *s)
+s32 animCmd_SetSpritePriority_BG(void *cursor, Sprite *s)
 {
     s->animCursor += AnimCommandSizeInWords(ACmd_SetSpritePriority);
     return 1;
 }
 
 // This is not a NOP-instruction in the regular version
-s32 animCmd_12_COPY(void *cursor, Sprite *s)
+s32 animCmd_12_BG(void *cursor, Sprite *s)
 {
     s->animCursor += AnimCommandSizeInWords(ACmd_SetOamOrder);
     return 1;
