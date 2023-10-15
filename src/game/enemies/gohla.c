518f74c21657fa792b935043b28a8e8589d4f8ab
Oliver Bell <freshollie@gmail.com>
2023-10-15 10:49:08 +0100

    finish gohla

--- src/game/enemies/gohla.c
+++ src/game/enemies/gohla.c
@@ -4,33 +4,41 @@
 #include "game/enemies/gohla.h"
 #include "malloc_vram.h"
 #include "sprite.h"
+#include "trig.h"
 
 #include "constants/animations.h"
 
 typedef struct {
     /* 0x00 */ SpriteBase base;
+
     /* 0x0C */ Sprite s;
     /* 0x3C */ Hitbox reserved;
-    /* 0x44 */ Sprite s2;
+
+    /* 0x44 */ Sprite proj;
+
     /* 0x74 */ s32 spawnX;
     /* 0x78 */ s32 spawnY;
     /* 0x7C */ s32 offsetX;
     /* 0x80 */ s32 offsetY;
-    /* 0x84 */ s32 unk84;
-    /* 0x88 */ s16 unk88;
-    /* 0x8A */ s16 unk8A;
-    /* 0x8C */ u8 unk8C;
+
+    /* 0x84 */ s32 projX;
+    /* 0x88 */ s16 projY;
+    /* 0x8A */ s16 projZ;
+
+    /* 0x8C */ bool8 unk8C;
 } Sprite_Gohla; /* size: 0x90 */
 
-void sub_8051928(void);
-
-void sub_8051E20(struct Task *);
+static void sub_8051928(void);
+static bool8 CheckPlayerHit(Sprite_Gohla *gohla, s32 x, s32 y);
+static void sub_8051AF0(void);
+static void RenderProjectiles(Sprite_Gohla *gohla, s16 x, s16 y, u32);
+static void TaskDestructor_Gohla(struct Task *);
 
 void CreateEntity_Gohla(MapEntity *me, u16 spriteRegionX, u16 spriteRegionY, u8 spriteY)
 {
     if (gGameMode == GAME_MODE_TIME_ATTACK || gDifficultyLevel != 1) {
-        struct Task *t
-            = TaskCreate(sub_8051928, sizeof(Sprite_Gohla), 0x4040, 0, sub_8051E20);
+        struct Task *t = TaskCreate(sub_8051928, sizeof(Sprite_Gohla), 0x4040, 0,
+                                    TaskDestructor_Gohla);
         Sprite_Gohla *gohla = TaskGetStructPtr(t);
         Sprite *s = &gohla->s;
         gohla->base.regionX = spriteRegionX;
@@ -40,9 +48,9 @@ void CreateEntity_Gohla(MapEntity *me, u16 spriteRegionX, u16 spriteRegionY, u8 
         gohla->base.spriteY = spriteY;
 
         if (me->d.sData[1]) {
-            gohla->unk8C = 1;
+            gohla->unk8C = TRUE;
         } else {
-            gohla->unk8C = 0;
+            gohla->unk8C = FALSE;
         }
 
         gohla->spawnX = Q_24_8(TO_WORLD_POS(me->x, spriteRegionX));
@@ -51,29 +59,23 @@ void CreateEntity_Gohla(MapEntity *me, u16 spriteRegionX, u16 spriteRegionY, u8 
         gohla->offsetY = Q_24_8(sub_801F07C(Q_24_8_TO_INT(gohla->spawnY),
                                             Q_24_8_TO_INT(gohla->spawnX), gohla->unk8C,
                                             8, NULL, sub_801EE64));
-        gohla->unk84 = 0;
-        gohla->unk88 = 0;
-        gohla->unk8A = 0;
+        gohla->projX = 0;
+        gohla->projY = 0;
+        gohla->projZ = 0;
 
         s->x = TO_WORLD_POS(me->x, spriteRegionX);
         s->y = TO_WORLD_POS(me->y, spriteRegionY);
         SET_MAP_ENTITY_INITIALIZED(me);
         SPRITE_INIT(s, 9, SA2_ANIM_GOHLA, 0, 18, 2);
 
-        s = &gohla->s2;
+        s = &gohla->proj;
         s->x = 0;
         s->y = 0;
         SPRITE_INIT(s, 6, SA2_ANIM_GOHLA_PROJ, 0, 17, 2);
     }
 }
 
-u8 sub_8051DC8(Sprite_Gohla *gohla);
-
-void sub_8051AF0(void);
-
-void sub_8051CB8(Sprite_Gohla *gohla, s16 x, s16 y, u32);
-
-void sub_8051928(void)
+static void sub_8051928(void)
 {
 #ifndef NON_MATCHING
     register IwramData ref asm("r1") = gCurTask->data;
@@ -94,30 +96,8 @@ void sub_8051928(void)
 
     ENEMY_CLAMP_TO_GROUND(gohla, gohla->unk8C);
 
-#ifndef NON_MATCHING
-    pos.x = ({
-        register s32 x asm("r1");
-        x = gohla->spawnX;
-        x += gohla->offsetX;
-        x = Q_24_8_TO_INT(x);
-        asm("" ::"r"(x));
-        x;
-    });
-    pos.y = ({
-        register s32 y asm("r2");
-        y = gohla->spawnY;
-        y += gohla->offsetY;
-        y = Q_24_8_TO_INT(y);
-        asm("" ::"r"(y));
-        y;
-    });
-    s->x = pos.x - gCamera.x;
-    s->y = pos.y - gCamera.y;
-#else
     ENEMY_UPDATE_POSITION(gohla, s, pos.x, pos.y);
-#endif
-
-    if (sub_8051DC8(gohla) != 0) {
+    if (CheckPlayerHit(gohla, pos.x, pos.y)) {
         TaskDestroy(gCurTask);
         return;
     }
@@ -130,5 +110,99 @@ void sub_8051928(void)
         s->prevVariant = -1;
     }
     ENEMY_UPDATE(s, pos.x, pos.y);
-    sub_8051CB8(gohla, s->x, s->y - 4, s->unk10);
+    RenderProjectiles(gohla, s->x, s->y - 4, s->unk10);
+}
+
+static void sub_8051AF0(void)
+{
+    Sprite_Gohla *gohla = TaskGetStructPtr(gCurTask);
+    Sprite *s = &gohla->s;
+    MapEntity *me = gohla->base.me;
+    Vec2_32 pos;
+
+    s32 delta = ENEMY_CLAMP_TO_GROUND_INNER(gohla, gohla->unk8C, sub_801EC3C);
+
+    if (delta < 0) {
+        gohla->offsetY += Q_24_8(delta);
+        delta = ENEMY_CLAMP_TO_GROUND_INNER(gohla, gohla->unk8C, sub_801EC3C);
+    }
+
+    if (delta > 0) {
+        gohla->offsetY += Q_24_8(delta);
+    }
+
+    ENEMY_UPDATE_POSITION(gohla, s, pos.x, pos.y);
+
+    if (CheckPlayerHit(gohla, pos.x, pos.y)) {
+        TaskDestroy(gCurTask);
+        return;
+    }
+
+    ENEMY_DESTROY_IF_OFFSCREEN(gohla, me, s);
+
+    sub_80122DC(Q_24_8_NEW(pos.x), Q_24_8_NEW(pos.y));
+    if (UpdateSpriteAnimation(s) == 0) {
+        ENEMY_TURN_AROUND(s);
+        s->graphics.anim = SA2_ANIM_GOHLA;
+        s->variant = 0;
+        s->prevVariant = -1;
+        gCurTask->main = sub_8051928;
+    }
+
+    DisplaySprite(s);
+    RenderProjectiles(gohla, s->x, s->y - 4, s->unk10);
+}
+
+static void RenderProjectiles(Sprite_Gohla *gohla, s16 x, s16 y, u32 flags)
+{
+    u8 i;
+    Sprite *proj = &gohla->proj;
+
+    UpdateSpriteAnimation(proj);
+
+    gohla->projY = (gohla->projY + 24) & ONE_CYCLE;
+    gohla->projZ = (gohla->projZ + 32) & ONE_CYCLE;
+
+    if (flags & SPRITE_FLAG_MASK_X_FLIP) {
+        SPRITE_FLAG_SET(proj, X_FLIP);
+    } else {
+        SPRITE_FLAG_CLEAR(proj, X_FLIP);
+    }
+
+    for (i = 0; i < 4; i++) {
+        u16 pos[2];
+        pos[0] = ((i * 256) + gohla->projY) & 0x3FF;
+        pos[1] = ((i * 384) + gohla->projZ) & 0x3FF;
+
+        proj->x = x + (COS(pos[0]) >> 10);
+        proj->y = y + (SIN(pos[1]) >> 13);
+
+        if (pos[0] >= 0x200) {
+            proj->unk1A = SPRITE_OAM_ORDER(17);
+        } else {
+            proj->unk1A = SPRITE_OAM_ORDER(19);
+        }
+        DisplaySprite(proj);
+    }
+}
+
+static bool8 CheckPlayerHit(Sprite_Gohla *gohla, s32 x, s32 y)
+{
+    Sprite *s = &gohla->s;
+    bool8 ret = FALSE;
+    if (sub_800C4FC(s, x, y, 0) != 0) {
+        ret = TRUE;
+    } else {
+        sub_800CA20(s, x, y, 0, &gPlayer);
+        sub_800CA20(s, x, y, 1, &gPlayer);
+    }
+
+    return ret;
+}
+
+static void TaskDestructor_Gohla(struct Task *t)
+{
+    Sprite_Gohla *gohla = TaskGetStructPtr(t);
+    VramFree(gohla->s.graphics.dest);
+    VramFree(gohla->proj.graphics.dest);
 }
