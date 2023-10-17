3b7530351f18349f58bb9f125debdecee5501618
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-10-17 13:04:51 +0200

    Match sub_80156D0

--- src/game/player_actions.c
+++ src/game/player_actions.c
@@ -7,12 +7,12 @@
 
 // TODO: Rename struct!
 typedef struct {
-    AnimId anim;
-    u16 variant;
-    s32 oldUnk10;
-    s32 moveState;
-    u16 unkC;
-    u8 animSpeed;
+    /* 0x00 */ AnimId anim;
+    /* 0x02 */ u16 variant;
+    /* 0x04 */ s32 flags;
+    /* 0x08 */ s32 moveState;
+    /* 0x0C */ u16 unkC;
+    /* 0x0E */ u8 animSpeed;
 } PlActions;
 
 extern PlActions gUnknown_030056B0[16];
@@ -28,7 +28,6 @@ void sub_801561C(void)
     u32 oldPlayerAnimSpeed = unk5A70->s.animSpeed;
     u32 oldPlayerUnk10 = unk5A70->s.unk10;
     u16 r6 = unk5A70->unk0[0];
-    u8 *first;
 
     oldPlayerMovestate &= ~MOVESTATE_80000000;
 
@@ -41,9 +40,32 @@ void sub_801561C(void)
         gUnknown_030056B0[i].variant = oldPlayerVariant;
         gUnknown_030056B0[i].moveState = oldPlayerMovestate;
         gUnknown_030056B0[i].animSpeed = oldPlayerAnimSpeed;
-        gUnknown_030056B0[i].oldUnk10 = oldPlayerUnk10;
+        gUnknown_030056B0[i].flags = oldPlayerUnk10;
         gUnknown_030056B0[i].unkC = r6;
     }
 
     gUnknown_03005830 = 0;
+}
+
+void sub_80156D0(void)
+{
+    Player *p = &gPlayer;
+    u32 oldMovestate = p->moveState;
+    u8 i;
+
+    gUnknown_03005830 = (gUnknown_03005830 + 1) & 0xF;
+    i = gUnknown_03005830;
+
+    oldMovestate &= ~MOVESTATE_80000000;
+
+    if (GRAVITY_IS_INVERTED) {
+        oldMovestate |= MOVESTATE_80000000;
+    }
+
+    gUnknown_030056B0[i].anim = p->anim;
+    gUnknown_030056B0[i].variant = p->variant;
+    gUnknown_030056B0[i].moveState = oldMovestate;
+    gUnknown_030056B0[i].animSpeed = p->unk90->s.animSpeed;
+    gUnknown_030056B0[i].flags = p->unk90->s.unk10;
+    gUnknown_030056B0[i].unkC = p->unk90->unk0[0];
 }
