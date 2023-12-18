f4b8521dade7c655fe5682c33d8d51ca969f87ab
JaceCear <11230293+JaceCear@users.noreply.github.com>
2023-12-18 15:26:01 +0100

    Format

--- src/bg_triangles_0.c
+++ src/bg_triangles_0.c
@@ -9,7 +9,8 @@
 #include "bg_triangles.h"
 
 // (99%) https://decomp.me/scratch/78CWY
-NONMATCH("asm/non_matching/engine/sub_8006228.inc", void sub_8006228(u8 bg, u8 param1, u8 param2, u8 param3, u8 param4, u8 param5))
+NONMATCH("asm/non_matching/engine/sub_8006228.inc",
+         void sub_8006228(u8 bg, u8 param1, u8 param2, u8 param3, u8 param4, u8 param5))
 {
     int_vcount *cursor;
     s16 r0, r1, r2, r4, r5, r7, r8;
