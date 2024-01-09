a3d1f9fc0fa15c2a5f7b162ec2b03db488541605
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-01-09 01:08:28 +0100

    Add include directive

--- include/game/multiplayer/indicators.h
+++ include/game/multiplayer/indicators.h
@@ -1,3 +1,9 @@
+#ifndef GUARD_SA2_MULTIPLAYER_INDICATORS_H
+#define GUARD_SA2_MULTIPLAYER_INDICATORS_H
+
+#include "global.h"
 
 extern void CreateSelfPositionIndicator(void);
 extern void CreateOpponentPositionIndicator(u8 sid);
+
+#endif // GUARD_SA2_MULTIPLAYER_INDICATORS_H
