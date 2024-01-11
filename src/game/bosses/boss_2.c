9fef73d99bdd064ee1923e6954e5559f8eb99539
Oliver Bell <freshollie@gmail.com>
2024-01-11 00:54:23 +0000

    more boss2

--- src/game/bosses/boss_2.c
+++ src/game/bosses/boss_2.c
@@ -53,11 +53,9 @@ typedef struct {
     /* 0xB0 */ Hitbox reserved;
 
     /* 0xB8 */ Sprite unkB8;
-    /* 0xB8 */ Sprite unkE8;
-    /* 0xB8 */ Sprite unk118;
+    /* 0xB8 */ Sprite unkE8[2];
     /* 0xB8 */ Sprite unk148;
-    /* 0xB0 */ Hitbox reserved2;
-    s32 filler180;
+    SpriteTransform transform;
     /* 0xB8 */ Sprite unk184;
 
 } EggBomberTank; /* size: 0x1B4 */
@@ -141,14 +139,14 @@ void CreateEggBomberTank(void)
     vram += 0x5A0;
     SPRITE_INIT_WITHOUT_VRAM(s, SA2_ANIM_EGG_BOMBER_TANK_BODY_PARTS, 0, 26, 2, 0);
 
-    s = &boss->unkE8;
+    s = &boss->unkE8[0];
     s->x = 0;
     s->y = 0;
     s->graphics.dest = vram;
     vram += 0x200;
     SPRITE_INIT_WITHOUT_VRAM(s, SA2_ANIM_EGG_BOMBER_TANK_WHEEL, 0, 22, 2, 0);
 
-    s = &boss->unk118;
+    s = &boss->unkE8[1];
     s->x = 0;
     s->y = 0;
     s->graphics.dest = vram;
@@ -273,7 +271,7 @@ void Task_803D088(void)
 }
 
 void sub_803D754(EggBomberTank *boss);
-void sub_803D640(EggBomberTank *boss, s32);
+void sub_803D640(EggBomberTank *boss, u8);
 void sub_803E214(EggBomberTank *boss);
 void sub_803E5B0(void);
 
@@ -321,6 +319,159 @@ void sub_803D368(EggBomberTank *boss)
             boss->unkC[idx][1] = boss->unkC[j][1] + 0x400;
             boss->unk3C[idx][0] = gUnknown_080D7B4E[idx][0];
             boss->unk3C[idx][1] = gUnknown_080D7B4E[idx][1];
+        }
+    }
+}
+
+u8 sub_803D430(EggBomberTank *boss)
+{
+    Sprite *s;
+    s8 i, j;
+    u8 ret;
+
+    s = &boss->unk80;
+    s->x = Q_24_8_TO_INT(boss->x) - gCamera.x;
+    s->y = (Q_24_8_TO_INT(boss->y) - gCamera.y) + 4;
+    UpdateSpriteAnimation(s);
+    DisplaySprite(s);
+
+    s = &boss->unkB8;
+    s->x = Q_24_8_TO_INT(boss->x) - gCamera.x;
+    s->y = (Q_24_8_TO_INT(boss->y) - gCamera.y) + 9;
+    UpdateSpriteAnimation(s);
+    DisplaySprite(s);
+
+    s = &boss->unk184;
+    s->x = (Q_24_8_TO_INT(boss->x) - gCamera.x) - 1;
+    s->y = (Q_24_8_TO_INT(boss->y) - gCamera.y) - 0x2A;
+    UpdateSpriteAnimation(s);
+    DisplaySprite(s);
+
+    for (i = 0; i < 2; i++) {
+        s8 temp;
+        s = &boss->unkE8[i];
+        UpdateSpriteAnimation(s);
+
+        temp = -12;
+
+        if (i == 0) {
+            temp = 0;
+        }
+
+        for (j = 0; j < 3; j++) {
+            s->x = ((Q_24_8_TO_INT(boss->x)) + gUnknown_080D7B10[j]) - gCamera.x + temp;
+            s->y = (Q_24_8_TO_INT(boss->unkC[j][1]) - gCamera.y) + 4;
+            DisplaySprite(s);
+        }
+    }
+
+    ret = 0;
+    if (boss->unk71 != 0) {
+        s32 temp2;
+        SpriteTransform *transform;
+        s = &boss->unk148;
+        transform = &boss->transform;
+        s->x = (Div(boss->x, 0x100) - gCamera.x) - 8;
+        s->y = (Div(boss->y, 0x100) - gCamera.y) - 0x16;
+
+        if (s->variant == 1) {
+            s->x -= Div(COS(boss->unk60) * boss->unk68, 25000);
+            s->y -= Div(SIN(boss->unk60) * boss->unk68, 25000);
+        }
+        s->unk10 = gUnknown_030054B8++ | 0x2060;
+
+        transform->rotation = boss->unk60;
+        transform->width = 0x100;
+        transform->height = 0x100;
+        transform->x = s->x;
+        transform->y = s->y;
+
+        if (UpdateSpriteAnimation(s) == 0) {
+            ret = 1;
+        }
+
+        sub_8004860(s, transform);
+        DisplaySprite(s);
+    }
+
+    return ret;
+}
+
+void sub_803D640(EggBomberTank *boss, u8 p1)
+{
+    Sprite *s;
+    s8 i, j;
+
+    s = &boss->unk80;
+    s->x = Q_24_8_TO_INT(boss->x) - gCamera.x;
+    s->y = (Q_24_8_TO_INT(boss->y) - gCamera.y) + 4;
+    UpdateSpriteAnimation(s);
+    DisplaySprite(s);
+
+    s = &boss->unkB8;
+    s->x = Q_24_8_TO_INT(boss->x) - gCamera.x;
+    s->y = (Q_24_8_TO_INT(boss->y) - gCamera.y) + 9;
+    UpdateSpriteAnimation(s);
+    DisplaySprite(s);
+
+    if (p1 != 0) {
+        s = &boss->unk184;
+        s->x = (Q_24_8_TO_INT(boss->x) - gCamera.x) - 1;
+        s->y = (Q_24_8_TO_INT(boss->y) - gCamera.y) - 0x2A;
+        UpdateSpriteAnimation(s);
+        DisplaySprite(s);
+    }
+
+    for (i = 0; i < 2; i++) {
+        s8 temp;
+        s = &boss->unkE8[i];
+        UpdateSpriteAnimation(s);
+
+        for (j = 0; j < 3; j++) {
+            s8 idx = j + (i * 3);
+            s->x = Q_24_8_TO_INT(boss->unkC[idx][0]) - gCamera.x;
+            s->y = (Q_24_8_TO_INT(boss->unkC[idx][1]) - gCamera.y) - 0x12;
+            DisplaySprite(s);
+        }
+    }
+}
+
+void sub_803D754(EggBomberTank *boss)
+{
+    s32 bottomX, bottomY, ground;
+    u8 i, j;
+
+    boss->unkA += 0x40;
+    boss->x += boss->unk8;
+    boss->y += boss->unkA;
+
+    ground = sub_801E4E4(Q_24_8_TO_INT(boss->y), Q_24_8_TO_INT(boss->x), 1, 8, NULL,
+                         sub_801EE64);
+
+    if (ground < 0) {
+        boss->y += Q_24_8_NEW(ground);
+        boss->unk8 -= 0x10;
+        if (boss->unk8 < 0) {
+            boss->unk8 = 0;
+        }
+        boss->unkA = Div(boss->unkA * -0x5A, 100);
+    }
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 3; j++) {
+            u8 idx = j + (i * 3);
+            boss->unk3C[idx][1] += 0x40;
+            boss->unkC[idx][0] += boss->unk3C[idx][0];
+            boss->unkC[idx][1] += boss->unk3C[idx][1];
+
+            ground
+                = sub_801E4E4(Q_24_8_TO_INT(boss->unkC[idx][1]) - 8,
+                              Q_24_8_TO_INT(boss->unkC[idx][0]), 1, 8, 0, sub_801EE64);
+            if (ground < 0) {
+                boss->unkC[idx][1] += Q_24_8_NEW(ground);
+                boss->unk3C[idx][0] -= 0x20;
+                boss->unk3C[idx][1] = Div(boss->unk3C[idx][1] * -0x50, 100);
+            }
         }
     }
 }
