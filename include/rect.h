aaebae1edb55158509b68af702ab36862c81eac9
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-02-02 18:55:43 +0100

    Add rectangle collision macros

---
+++ include/rect.h
@@ -0,0 +1,36 @@
+#ifndef GUARD_RECT_H
+#define GUARD_RECT_H
+
+struct Rect8 {
+    /* 0x00 */ s8 left;
+    /* 0x01 */ s8 top;
+    /* 0x02 */ s8 right;
+    /* 0x03 */ s8 bottom;
+};
+
+#define RECT_WIDTH(hb)     ((hb)->right - (hb)->left)
+#define RECT_HEIGHT(hb)    ((hb)->bottom - (hb)->top)
+#define RECT_LEFT(x, hb)   ((x) + (hb)->left)
+#define RECT_RIGHT(x, hb)  (RECT_LEFT(x, hb) + RECT_WIDTH(hb))
+#define RECT_TOP(y, hb)    ((y) + (hb)->top)
+#define RECT_BOTTOM(y, hb) (RECT_TOP(y, hb) + RECT_HEIGHT(hb))
+#define RECT_COLLISION_A_X(x0, hb0, x1, hb1)                                            \
+    (RECT_LEFT(x0, hb0) <= RECT_LEFT(x1, hb1)                                           \
+     && RECT_RIGHT(x0, hb0) >= RECT_LEFT(x1, hb1))
+#define RECT_COLLISION_B_X(x0, hb0, x1, hb1)                                            \
+    (RECT_LEFT(x0, hb0) >= RECT_LEFT(x1, hb1)                                           \
+     && RECT_RIGHT(x1, hb1) >= RECT_LEFT(x0, hb0))
+#define RECT_COLLISION_A_Y(y0, hb0, y1, hb1)                                            \
+    (RECT_TOP((y0), hb0) <= RECT_TOP((y1), hb1)                                         \
+     && RECT_BOTTOM((y0), hb0) >= RECT_TOP((y1), hb1))
+#define RECT_COLLISION_B_Y(y0, hb0, y1, hb1)                                            \
+    (RECT_TOP((y0), hb0) >= RECT_TOP((y1), hb1)                                         \
+     && RECT_BOTTOM((y1), hb1) >= RECT_TOP((y0), hb0))
+#define RECT_COLLISION_X(x0, hb0, x1, hb1)                                              \
+    (RECT_COLLISION_A_X(x0, hb0, x1, hb1) || RECT_COLLISION_B_X(x0, hb0, x1, hb1))
+#define RECT_COLLISION_Y(y0, hb0, y1, hb1)                                              \
+    (RECT_COLLISION_A_Y(y0, hb0, y1, hb1) || RECT_COLLISION_B_Y(y0, hb0, y1, hb1))
+#define RECT_COLLISION(x0, y0, hb0, x1, y1, hb1)                                        \
+    (RECT_COLLISION_X(x0, hb0, x1, hb1) && RECT_COLLISION_Y(y0, hb0, y1, hb1))
+
+#endif // GUARD_RECT_H
--- src/sakit/entities_0.c
+++ src/sakit/entities_0.c
@@ -1,37 +1,25 @@
 #include "global.h"
+#include "rect.h"
 #include "sprite.h"
 #include "sakit/player.h"
 
-u32 sub_800BFAC(Sprite *s, s32 x, s32 y, Player *p, s8 *param4) {
+u32 sub_800BFAC(Sprite *s, s32 x, s32 y, Player *p, struct Rect8 *param4)
+{
     s32 left, right, top;
-    s32 r1, r3;
+
+    s32 px;
 
     u32 result = 0;
-    
-    if((s->hitboxes[0].index == -1) || !IS_ALIVE(p)) {
+
+    if ((s->hitboxes[0].index == -1) || !IS_ALIVE(p)) {
         return result;
     }
 
-    left = x + s->hitboxes[0].left;
-
-    r1 = Q_24_8_TO_INT(p->x) + param4[0];
-
-    if(((left <= r1)
-    && ((left + (s->hitboxes[0].right - s->hitboxes[0].left)) >= r1))
-    || ((left >= r1) && ((r1 + (param4[2] - param4[0])) >= left))) {
-        // _0800C010
-        top = y + s->hitboxes[0].top;
-        r3  = Q_24_8_TO_INT(p->y) + param4[1];
-
-        if((top > r3) 
-        || ((top + (s->hitboxes[0].bottom - s->hitboxes[0].top)) < r3)) {
-            if((top >= r3) && (r3 + (param4[3] - param4[1])) >= top) {
-                result |= 0x80000;
-            }
-        } else {
-            result |= 0x80000;
-        }
+    if (RECT_COLLISION(x, y, &s->hitboxes[0], Q_24_8_TO_INT(p->x), Q_24_8_TO_INT(p->y),
+                       param4)) {
+        result |= 0x80000;
     }
 
     return result;
 }
+asm(".short 0");
