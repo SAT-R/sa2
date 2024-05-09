4555473aadf93a783464c6a2e089964b48fdc99e
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-05-10 00:08:02 +0200

    Formatter...

--- src/lib/libagbsyscall.c
+++ src/lib/libagbsyscall.c
@@ -5,16 +5,18 @@
 
 // Reimplementation of libagbsyscall for non-GBA platforms
 
-inline s32 Div(s32 num, s32 denom) {
-    if(denom != 0) {
+inline s32 Div(s32 num, s32 denom)
+{
+    if (denom != 0) {
         return num / denom;
     } else {
         return 0;
     }
 }
 
-inline s32 Mod(s32 num, s32 denom) {
-    if(denom != 0) {
+inline s32 Mod(s32 num, s32 denom)
+{
+    if (denom != 0) {
         return num % denom;
     } else {
         return 0;
