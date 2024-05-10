6866d6906dae22f078a96936574b11cf698a6e50
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-05-10 22:23:40 +0200

    Move debug_text_printer.h

--- include/game/stage/debug_text_printer.h
+++ include/game/stage/debug_text_printer.h
@@ -4,5 +4,6 @@
 extern struct Task *Debug_CreateAsciiTask(s16 x, s16 y);
 extern void Debug_PrintIntegerAt(u32 value, u16 x, u16 y);
 extern void Debug_PrintTextAt(char *value, u16 x, u16 y);
+extern void Debug_TextPrinterDestroy();
 
 #endif // GUARD_STAGE_DEBUG_TEXT_PRINTER_H
