c68738dcdeba8d7dd9a88624611e8a2433fbb5aa
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-05-16 18:20:44 +0200

    Fix non-vanilla refs in Chao Garden

--- chao_garden/songs.mk
+++ chao_garden/songs.mk
@@ -1,8 +1,8 @@
 STD_REVERB = 0
 
 $(MID_BUILDDIR)/%.o: $(MID_SUBDIR)/%.s
-	@echo "$(AS) <flags> -I sound -o $@ $<"
-	@$(AS) $(ASFLAGS) -I sound -o $@ $<
+	@echo "$(PREPROC) $< | $(ASM_PSEUDO_OP_CONV) | $(CPP) $(CPPFLAGS) - | $(AS) <flags> -I sound -o $@ $<"
+	@$(PREPROC) $< "" | $(ASM_PSEUDO_OP_CONV) | $(CPP) $(CPPFLAGS) - | $(AS) $(ASFLAGS) -o $@
 
 $(MID_SUBDIR)/mus_garden.s: %.s: %.mid
 	$(MID2AGB) $< $@ -E -R$(STD_REVERB) -G0  -V100
