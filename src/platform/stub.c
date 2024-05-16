3a95f751b567f525e6e89b8b05fe181657280727
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-05-16 15:02:01 +0200

    Add Boss_7 stubs

--- src/platform/stub.c
+++ src/platform/stub.c
@@ -26,9 +26,59 @@ u32 sub_800D0A0(Sprite *s, s16 x, s16 y, s16 param3, s16 param4, u8 param5, u32 
 void sub_802E784(u16 p0, u16 p1, u16 p2, s16 x, s16 y, s16 p5) { }
 
 // boss_7
+typedef void (*EggFrogCallback)(void *);
+
+typedef struct {
+    u8 unk0[6];
+    u8 unk6;
+    u8 unk7;
+    u8 filler8[6];
+    u8 unkE;
+    u8 unkF;
+    u8 filler0[2];
+    u8 unk12;
+    u8 unk13;
+    u8 filler1[6];
+    u8 unk1A;
+    u8 unk1B;
+    u8 filler2[6];
+    u8 unk22;
+    u8 unk23;
+    u8 filler3[10];
+    u8 unk2E;
+    u8 unk2F;
+    u8 unk[6];
+    u8 unk36;
+    u8 unk37;
+    u16 unk38;
+    u8 unk3A;
+    u8 unk3B;
+} UNK_80D8710;
+const s8 gUnknown_080D8148[2];
+const s8 gUnknown_080D814A[0x452];
+const u32 gUnknown_080D86D4[15];
+const TileInfo gUnknown_080D8108[8] = { 0 };
+const UNK_80D8710 gUnknown_080D8710 = { 0 };
+const u8 gUnknown_080D8788[][7] = { { 0, 0, 0, 0, 0, 0, 0 } };
+void Task_EggFrogMain(void) {};
+void sub_8048F44(void) {};
+void TaskDestructor_EggFrogMain(struct Task *t) { }
+void sub_8048C7C(void *boss) { } // actually: (EggFrog *boss);
+void sub_8048D78(void *boss) { } // actually: (EggFrog *boss);
+void sub_8048E64(void *boss) { } // actually: (EggFrog *boss);
+void sub_804928C(void *boss) { } // actually: (EggFrog *boss);
+void sub_80492B8(void *boss) { } // actually: (EggFrog *boss);
+void sub_804931C(void *boss) { } // actually: (EggFrog *boss);
+void sub_80493F8(void *boss, s32 x, s32 y, u8 param3) {
+} // actually: (EggFrog *boss, s32 x, s32 y, u8 );
 void sub_80498CC(u8 param0) { }
+const void *const gUnknown_080D874C[15]
+    = { sub_8048C7C, sub_8048C7C, sub_8048C7C, sub_8048C7C, sub_8048C7C,
+        sub_8048C7C, sub_8048C7C, sub_8048C7C, sub_8048C7C, sub_8048C7C,
+        sub_8048C7C, sub_8048C7C, sub_8048C7C, sub_8048C7C };
 
 // boss_9
+const u16 *const gUnknown_080D859C[39] = {};
 const s16 gUnknown_080D8A94[4] = { 0 };
 const s16 gUnknown_080D8AAC[4] = { 0 };
 const s16 gUnknown_080D8AC4[4] = { 0 };
@@ -55,7 +105,6 @@ void sub_8084B54(struct UNK_8085F1C_1 *p0, u16 p1, u16 p2, u16 p3) { }
 
 // TEMP
 void sub_8048EB4(s32 a, s32 b) { }
-void CreateEggFrog(void) { }
 void CreateSuperEggRoboZ(void) { }
 void RealClearChain(void) { }
 void SoundMainBTM(void) { }
