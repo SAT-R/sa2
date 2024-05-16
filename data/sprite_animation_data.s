f02fefdc5fb9a3b1062aaa9d56b8273b39f58cb4
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-05-16 15:01:22 +0200

    Introduce C_DECL macro to labels that get cross-referenced between ASM <-> C

--- data/sprite_animation_data.s
+++ data/sprite_animation_data.s
@@ -1,17 +1,19 @@
-	.section .rodata
+.include "asm/macros/c_decl.inc"
+
+.section .rodata
 
 @ TODO: move these into C files in src/data?
-    .global gSpritePalettes @ 0x0816ADC8
-gSpritePalettes:
+    .global C_DECL(gSpritePalettes) @ 0x0816ADC8
+C_DECL(gSpritePalettes):
     .include "graphics/obj_palettes.inc"
 
-    .global gObjTiles_4bpp @ 0x081709A8
-gObjTiles_4bpp:
+    .global C_DECL(gObjTiles_4bpp) @ 0x081709A8
+C_DECL(gObjTiles_4bpp):
     .include "graphics/obj_tiles_4bpp.inc"
 
 @ Unlike the first game, Sonic Advance 2 does not use 8-bits-per-pixel object tiles,
 @   but the engine does need a dummy-pointer to 8bpp data!
 @   Here it would just turn out to be "garbage" data.
-    .global gObjTiles_8bpp
-gObjTiles_8bpp: @ 0x086E9E08
+    .global C_DECL(gObjTiles_8bpp)
+C_DECL(gObjTiles_8bpp): @ 0x086E9E08
     @ DUMMY - NO DATA HERE!
