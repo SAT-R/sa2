143b820de3ee2bda77b75430412c9595f422292d
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-05-17 23:01:56 +0200

    Move some of the memory decls around

--- include/gba/defines.h
+++ include/gba/defines.h
@@ -18,14 +18,43 @@
 #define ALIGNED(n) __attribute__((aligned(n)))
 // #endif
 
+#define EWRAM_SIZE  0x40000
+#define IWRAM_SIZE  0x7E00
+#define PLTT_SIZE   0x400
+#define BG_PLTT_SIZE 0x200
+#define OBJ_PLTT_SIZE 0x200
+#define VRAM_SIZE   0x18000
+#define BG_VRAM_SIZE      0x10000
+#define OAM_ENTRY_COUNT 128
 #if PORTABLE
+#include "gba/types.h"
+#define OAM_SIZE (OAM_ENTRY_COUNT*sizeof(OamData))
 // NOTE(Jace): For this to work either "gba/types.h" needs to be
 //             included here, or this has to go into a different file.
 //             Including types.h here, has the sideeffect of not generating
-//             a non-matching ROM, and increasing the GBA ROM size by ~24 kilobytes
+//             a matching ROM, and increasing the GBA ROM size by ~24 kilobytes
 extern struct SoundInfo *SOUND_INFO_PTR;
 extern uint16_t INTR_CHECK;
 extern void (*INTR_VECTOR)(void);
+
+extern uint8_t EWRAM_START[EWRAM_SIZE];
+extern uint8_t IWRAM_START[IWRAM_SIZE];
+extern uint16_t PLTT[PLTT_SIZE/2];
+#define BG_PLTT &PLTT[0]
+#define OBJ_PLTT &PLTT[BG_PLTT_SIZE/2]
+extern uint8_t OAM[OAM_SIZE];
+extern uint8_t VRAM[VRAM_SIZE];
+
+#define BG_VRAM           &VRAM[0]
+#define BG_CHAR_ADDR(n)   (((u8*)BG_VRAM) + (0x4000 * (n)))
+#define BG_SCREEN_ADDR(n) (((u8*)BG_VRAM) + (0x800 * (n)))
+#define BG_TILE_ADDR(n)   (((u8*)BG_VRAM) + (0x80 * (n)))
+
+#define OBJ_VRAM0         &VRAM[0x10000]
+#define OBJ_VRAM1         &VRAM[0x14000]
+
+#define DISPLAY_WIDTH  240
+#define DISPLAY_HEIGHT 160
 #else
 // NOTE(Jace): I tried replacing these altogether,
 //             but that resulted in a nonmatching ROM
@@ -33,51 +62,43 @@ extern void (*INTR_VECTOR)(void);
 #define SOUND_INFO_PTR (*(struct SoundInfo **)0x3007FF0)
 #define INTR_CHECK     (*(u16 *)0x3007FF8)
 #define INTR_VECTOR    (*(void **)0x3007FFC)
-#endif
 
 #define EWRAM_START 0x02000000
-#define EWRAM_SIZE  0x40000
 #define IWRAM_START 0x03000000
-#define IWRAM_SIZE  0x7E00
 
-#define PLTT      0x5000000
-#define PLTT_SIZE 0x400
-
+#define PLTT         0x5000000
 #define BG_PLTT      PLTT
-#define BG_PLTT_SIZE 0x200
-
-#define OBJ_PLTT      (PLTT + 0x200)
-#define OBJ_PLTT_SIZE 0x200
+#define OBJ_PLTT      (PLTT + BG_PLTT_SIZE)
 
 #define VRAM      0x6000000
-#define VRAM_SIZE 0x18000
 
 #define BG_VRAM           VRAM
-#define BG_VRAM_SIZE      0x10000
 #define BG_CHAR_ADDR(n)   (BG_VRAM + (0x4000 * (n)))
 #define BG_SCREEN_ADDR(n) (BG_VRAM + (0x800 * (n)))
 #define BG_TILE_ADDR(n)   (BG_VRAM + (0x80 * (n)))
 
 // text-mode BG
-#define OBJ_VRAM0      (VRAM + 0x10000)
+#define OBJ_VRAM0      (u8*)(VRAM + 0x10000)
 #define OBJ_VRAM0_SIZE 0x8000
 
 // bitmap-mode BG
-#define OBJ_VRAM1      (VRAM + 0x14000)
+#define OBJ_VRAM1      (u8*)(VRAM + 0x14000)
 #define OBJ_VRAM1_SIZE 0x4000
 
 #define OAM      0x7000000
-#define OAM_ENTRY_COUNT 128
 #define OAM_SIZE (OAM_ENTRY_COUNT*sizeof(OamData))
 
 #define DISPLAY_WIDTH  240
 #define DISPLAY_HEIGHT 160
+#endif
 
 #define TILE_SIZE_4BPP 32
 #define TILE_SIZE_8BPP 64
 
 // NOTE/TODO: Maybe this should somewhere else?
-#define GET_TILE_NUM_COMMON(vramPtr, tileSize) (((size_t)(vramPtr) - OBJ_VRAM0) / (tileSize))
+// NOTE: This appears to not match when using pointers, but with integers it's fine.
+//       uintptr_t should always be defined to be as big as a pointer, so there should be no issues.
+#define GET_TILE_NUM_COMMON(vramPtr, tileSize) (((uintptr_t)(vramPtr) - (uintptr_t)OBJ_VRAM0) / (tileSize))
 #define GET_TILE_NUM(vramPtr) GET_TILE_NUM_COMMON((vramPtr), TILE_SIZE_4BPP) 
 
 #define TOTAL_OBJ_TILE_COUNT 1024
