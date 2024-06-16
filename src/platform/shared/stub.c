43b4bd8fa735d75f287a53038b2bb3313e219020
Oliver Bell <freshollie@gmail.com>
2024-06-16 23:14:02 +0100

    fix stubs

--- src/platform/shared/stub.c
+++ src/platform/shared/stub.c
@@ -35,7 +35,7 @@ const u32 gUnknown_080D8874[] = { 0 };
 const TileInfo gUnknown_080D8864[] = { { 0, 0, 0 } };
 const s32 gUnknown_080D8840[][2] = { { 0, 0 } };
 
-void sub_804A6B4(struct Task *) {};
+void sub_804A6B4(struct Task *t) {};
 
 // boss_9
 const s16 gUnknown_080D8A94[4] = { 0 };
