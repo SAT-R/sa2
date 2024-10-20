207d4f6978255ca6ee84eb4e5f32ef09e3a325a4
Oliver Bell <freshollie@gmail.com>
2024-10-20 18:53:22 +0100

    more results screen fixes for widescreen, refactor results screens to share struct

--- include/game/stage/results.h
+++ include/game/stage/results.h
@@ -2,11 +2,27 @@
 #define GUARD_STAGE_RESULTS_H
 
 #include "global.h"
+#include "sprite.h"
+#include "screen_fade.h"
+
+typedef struct {
+    /*  0x00 */ ScreenFade fade;
+    /*  0x0C */ Sprite title[3];
+    /*  0x9C */ Sprite sprScores[3];
+    /* 0x12C */ Sprite separator;
+    /* 0x15C */ u32 timeBonusScore;
+    /* 0x160 */ u32 ringBonusScore;
+    /* 0x164 */ u32 spRingBonusScore;
+    /* 0x168 */ s32 counter; // frames since task started
+    /* 0x16C */ s32 unk16C;
+} StageResultsBase; /* size: 0x174 */
 
 u16 CreateStageResults(u32, u16, u8);
+void StageResults_AnimateSeparator(void);
+void StageResults_AnimateTitle(void);
 
-extern const u16 sAnimsGotThroughZoneAndActNames[11][3];
-extern const u16 sStageResultsHeadlineTexts[5][3];
-extern const u16 sAnimsGotThroughCharacterNames[5][3];
+extern const u16 gAnimsGotThroughZoneAndActNames[][3];
+extern const u16 gStageResultsHeadlineTexts[][3];
+extern const u16 gAnimsGotThroughCharacterNames[][3];
 
 #endif // GUARD_STAGE_RESULTS_H
