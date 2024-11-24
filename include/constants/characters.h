894144191550f5f626a670d8bc63a94c913df659
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-11-24 11:07:30 +0100

    Fix BriBaSA_ex not showing the correct animation for many entities

--- include/constants/characters.h
+++ include/constants/characters.h
@@ -1,14 +1,17 @@
 #ifndef GUARD_CONSTANTS_CHARACTERS_H
 #define GUARD_CONSTANTS_CHARACTERS_H
 
-#define CHARACTER_SHARED_ANIM 0
-#define CHARACTER_SONIC       0
-#define CHARACTER_CREAM       1
-#define CHARACTER_TAILS       2
-#define CHARACTER_KNUCKLES    3
-#define CHARACTER_AMY         4
+#define CHARACTER_SONIC    0
+#define CHARACTER_CREAM    1
+#define CHARACTER_TAILS    2
+#define CHARACTER_KNUCKLES 3
+#define CHARACTER_AMY      4
 
 #define NUM_CHARACTERS 5
+
+// NOTE: Put this after NUM_CHARACTERS, to allow BriBaSA_ex to parse this file properly.
+//       Naming it CHARACTER_<something> before NUM_CHARACTERS confuses it.
+#define CHARACTER_SHARED_ANIM 0
 
 #define CHARACTER_BIT(character) (1 << (character))
 
