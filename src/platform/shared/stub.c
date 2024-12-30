53c8ade188547d5a447cef7063c6e79f92ad62d5
JaceCear <11230293+JaceCear@users.noreply.github.com>
2024-12-30 14:46:35 +0100

    Remove sub_800D0A0 from stub.c

--- src/platform/shared/stub.c
+++ src/platform/shared/stub.c
@@ -2,16 +2,12 @@
 
 #include "global.h"
 #include "sprite.h"
-#include "game/sa1_sa2_shared/collision.h" // sub_800D0A0
 #include "game/bosses/boss_5.h" // sub_802E784
 
 void *ewram_end = 0;
 void *iwram_end = 0;
 char gNumMusicPlayers = 4;
 int gMaxLines = 0;
-
-// collision
-u32 sub_800D0A0(Sprite *s, s16 x, s16 y, s16 param3, s16 param4, u8 param5, u32 param6) { return 0; }
 
 // boss_5
 void sub_802E784(u16 p0, u16 p1, u16 p2, s16 x, s16 y, s16 p5) { }
