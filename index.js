32c7a922d7a0bf7816f4d0f351c6427cef3b13b6
Oliver Bell <freshollie@gmail.com>
2025-01-10 11:37:32 +0000

    update chart to show matching

--- index.js
+++ index.js
@@ -7,23 +7,28 @@ const headings = [
   "dataBytes",
   "dataTotalBytes",
 ];
-window.onload = async () => {
-  const reportCsv = await (
-    await fetch("./reports/progress-sa2-nonmatching.csv")
-  ).text();
-  let matchingData = reportCsv
+
+const fetchReport = async (path) => {
+  const reportCsv = await (await fetch(path)).text();
+  return reportCsv
     .split("\n")
     .filter(Boolean)
     .map((line) =>
       Object.fromEntries(
         line.split(",").map((value, i) => [headings[i], value])
       )
-    );
-  
-  matchingData = matchingData.map((item) => ({
-    ...item,
-    time: new Date(parseInt(item.time * 1000)).toISOString(),
-  }));
+    )
+    .map((item) => ({
+      ...item,
+      time: new Date(parseInt(item.time * 1000)).toISOString(),
+    }));
+};
+
+window.onload = async () => {
+  const [nonMatchingData, matchingData] = await Promise.all([
+    fetchReport("./reports/progress-sa2-nonmatching.csv"),
+    fetchReport("./reports/progress-sa2-matching.csv"),
+  ]);
 
   const ctx = document.getElementById("progress-chart").getContext("2d");
   const myChart = new Chart(ctx, {
@@ -32,33 +37,44 @@ window.onload = async () => {
       datasets: [
         {
           label: "Decompiled",
+          data: nonMatchingData.map((item) => ({
+            x: item.time,
+            y: item.bytes / item.totalBytes,
+            commit: item.commit,
+          })),
+          borderColor: "rgb(75, 192, 192)",
+        },
+        {
+          label: "Matching",
           data: matchingData.map((item) => ({
             x: item.time,
-            y: (item.bytes / item.totalBytes),
-            commit: item.commit
+            y: item.bytes / item.totalBytes,
+            commit: item.commit,
           })),
-          borderColor: 'rgb(75, 192, 192)',
+          borderColor: "#23ff00",
         },
       ],
     },
     options: {
       interaction: {
-        mode: 'index',
-        axis: 'y'
-    },
+        mode: "index",
+        axis: "y",
+      },
       scales: {
-        
         y: {
           min: 0,
           ticks: {
-                format: {
-                    style: 'percent'
-                }
+            format: {
+              style: "percent",
             },
-            precision: 2,
+          },
+          precision: 2,
         },
         x: {
           type: "time",
+          time: {
+            unit: "month",
+          },
           // time: {
           //   displayFormats: {
           //       quarter: 'MMM YYYY'
