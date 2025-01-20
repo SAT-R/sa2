11726da49eb44e9b327bfcab14332430e3ff0d47
heyjoeway <joe@jojudge.com>
2025-01-20 16:01:20 -0500

    Document Player_AirMovement

--- include/game/stage/player.h
+++ include/game/stage/player.h
@@ -23,7 +23,7 @@ void Player_TransitionCancelFlyingAndBoost(Player *p);
 void sub_8023B5C(Player *, s32);
 void sub_8023260(Player *);
 void sub_80232D0(Player *);
-void sub_8023610(Player *);
+void Player_AirMovement(Player *);
 void Player_TouchGround(Player *p);
 void Player_Uncurl(Player *p);
 void Player_HandlePhysicsWithAirInput(Player *p);
