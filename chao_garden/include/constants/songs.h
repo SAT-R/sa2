018ddc51daa5c9c95e9e73f369fe9e0aca4a9441
JaceCear <11230293+JaceCear@users.noreply.github.com>
2025-02-17 13:47:14 +0100

    Document Chao Garden song indices (#204)

---
+++ chao_garden/include/constants/songs.h
@@ -0,0 +1,42 @@
+#ifndef GUARD_CONSTANTS_SONGS_H
+#define GUARD_CONSTANTS_SONGS_H
+
+#define MUS_DUMMY             0
+#define MUS_GARDEN            1
+#define MUS_GOODBYE_CHAO      2
+#define MUS_DUMMY_3           3
+#define MUS_MINIGAME          4
+#define MUS_DUMMY_5           5
+#define MUS_DUMMY_6           6
+#define MUS_DUMMY_7           7
+#define MUS_DUMMY_8           8
+#define MUS_DUMMY_9           9
+#define MUS_DUMMY_10          10
+#define SE_MSGBOX_SELECT      11
+#define SE_CONFIRM            12
+#define SE_13                 13
+#define SE_MSGBOX             14
+#define SE_CHAO_WALK          15
+#define SE_16                 16
+#define SE_17                 17
+#define SE_18                 18
+#define SE_19                 19
+#define SE_SNORE              20
+#define SE_21                 21
+#define SE_GAUGE_EXPANDS      22
+#define SE_MINIGAME_LOST      23
+#define SE_24                 24
+#define SE_25                 25
+#define SE_TOY_TRUMPET        26
+#define SE_27                 27
+#define SE_28                 28
+#define SE_29                 29
+#define SE_30                 30
+#define SE_SPEECH_BUBBLE      31
+#define SE_32                 32
+#define SE_HATCH              33
+#define SE_34                 34
+#define SE_GAUGE_LVL_UP       35
+#define SE_TOY_TRUMPET_NOVICE 36
+
+#endif // GUARD_CONSTANTS_SONGS_H
