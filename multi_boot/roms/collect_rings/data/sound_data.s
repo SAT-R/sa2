6ec009d66fc84d3308d5e04a9a08fd1afc0709ac
Oliver Bell <freshollie@gmail.com>
2025-06-16 22:42:24 +0100

    collect_rings: match extracted sound data

--- multi_boot/roms/collect_rings/data/sound_data.s
+++ multi_boot/roms/collect_rings/data/sound_data.s
@@ -1,16 +1,160 @@
-	.section .rodata
+.include "asm/macros/c_decl.inc"
+.include "asm/macros/portable.inc"
+.include "../../../asm/macros/m4a.inc"
+.include "../../../asm/macros/music_voice.inc"
 
-.global voicegroup000
-    .incbin "data/rom_data.bin", 0x00016084, 0xC18
+.section .rodata
 
-    .global gMPlayTable
-gMPlayTable:
-    .incbin "data/rom_data.bin", 0x00016C9C, 0x30
+.include "sound/voicegroups/voicegroup000.inc"
+.include "sound/voicegroups/voicegroup001.inc"
+.include "sound/voicegroups/voicegroup002.inc"
+	.align 2
+	.global ProgrammableWaveData_2016CCC
+ProgrammableWaveData_2016CCC:
+	.incbin "sound/programmable_wave_samples/02016CCC.pcm"
 
-    .global gSongTable
-gSongTable:
-    .incbin "data/rom_data.bin", 0x00016CCC, 0x5A34
+	.align 2
+	.global ProgrammableWaveData_2016CDC
+ProgrammableWaveData_2016CDC:
+	.incbin "sound/programmable_wave_samples/02016CDC.pcm"
 
-    .global se_ring_copy
-se_ring_copy:
-    .incbin "data/rom_data.bin", 0x0001C700, 0x46C
+	.align 2
+	.global ProgrammableWaveData_2016CEC
+ProgrammableWaveData_2016CEC:
+	.incbin "sound/programmable_wave_samples/02016CEC.pcm"
+
+	.align 2
+	.global ProgrammableWaveData_2016CFC
+ProgrammableWaveData_2016CFC:
+	.incbin "sound/programmable_wave_samples/02016CFC.pcm"
+
+	.align 2
+	.global ProgrammableWaveData_2016D0C
+ProgrammableWaveData_2016D0C:
+	.incbin "sound/programmable_wave_samples/02016D0C.pcm"
+
+	.align 2
+	.global ProgrammableWaveData_2016D1C
+ProgrammableWaveData_2016D1C:
+	.incbin "sound/programmable_wave_samples/02016D1C.pcm"
+
+	.align 2
+	.global ProgrammableWaveData_2016D2C
+ProgrammableWaveData_2016D2C:
+	.incbin "sound/programmable_wave_samples/02016D2C.pcm"
+
+	.align 2
+	.global ProgrammableWaveData_2016D3C
+ProgrammableWaveData_2016D3C:
+	.incbin "sound/programmable_wave_samples/02016D3C.pcm"
+
+	.align 2
+	.global ProgrammableWaveData_2016D4C
+ProgrammableWaveData_2016D4C:
+	.incbin "sound/programmable_wave_samples/02016D4C.pcm"
+
+	.align 2
+	.global gMPlayTable
+gMPlayTable:	@ 0x02016D5C
+	music_player 0x201F270, 0x201CD30, 16, 0
+	music_player 0x201F2B0, 0x201D230, 16, 0
+	music_player 0x201F2F0, 0x201D730, 16, 0
+	music_player 0x201F340, 0x201DC30, 16, 0
+
+	.align 2
+	.global gSongTable
+gSongTable:	@ 0x02016D8C
+	song song000, 0, 0
+	song song001, 0, 0
+	song song002, 0, 0
+	song song003, 0, 0
+	song song004, 1, 1
+	song song005, 0, 0
+	song song000, 0, 0
+	song song000, 0, 0
+	song song000, 0, 0
+	song song000, 0, 0
+	song song010, 2, 2
+	song song011, 2, 2
+	song song012, 2, 2
+	song song013, 2, 2
+	song song014, 2, 2
+	song se_ring_copy, 3, 3
+	song song016, 3, 3
+	song song017, 2, 2
+	song song018, 2, 2
+	song song019, 2, 2
+	song song020, 2, 2
+	song song021, 2, 2
+	song song022, 2, 2
+	song song023, 2, 2
+	song song024, 2, 2
+	song song025, 2, 2
+	song song026, 2, 2
+
+	.align 2
+	.global song000
+song000:	@ 0x02016E64
+	.byte	0		@ trackCount
+	.byte	0		@ blockCount
+	.byte	0		@ priority
+	.byte	0		@ reverb
+
+
+	.align 2
+	.global gUnknown_02016E68
+gUnknown_02016E68:
+	.incbin "sound/direct_sound_samples/02016E68.bin"
+
+	.align 2
+	.global gUnknown_02017368
+gUnknown_02017368:
+	.incbin "sound/direct_sound_samples/02017368.bin"
+
+	.align 2
+	.global gUnknown_020178A0
+gUnknown_020178A0:
+	.incbin "sound/direct_sound_samples/020178A0.bin"
+
+	.align 2
+	.global gUnknown_02017D58
+gUnknown_02017D58:
+	.incbin "sound/direct_sound_samples/02017D58.bin"
+
+	.align 2
+	.global gUnknown_02018260
+gUnknown_02018260:
+	.incbin "sound/direct_sound_samples/02018260.bin"
+
+	.align 2
+	.global gUnknown_020186F4
+gUnknown_020186F4:
+	.incbin "sound/direct_sound_samples/020186F4.bin"
+
+	.align 2
+	.global gUnknown_020197C8
+gUnknown_020197C8:
+	.incbin "sound/direct_sound_samples/020197C8.bin"
+
+.include "sound/songs/song001.s"
+.include "sound/songs/song002.s"
+.include "sound/songs/song003.s"
+.include "sound/songs/song004.s"
+.include "sound/songs/song005.s"
+.include "sound/songs/song010.s"
+.include "sound/songs/song011.s"
+.include "sound/songs/song012.s"
+.include "sound/songs/song013.s"
+.include "sound/songs/song014.s"
+.include "sound/songs/se_ring_copy.s"
+.include "sound/songs/song016.s"
+.include "sound/songs/song017.s"
+.include "sound/songs/song018.s"
+.include "sound/songs/song019.s"
+.include "sound/songs/song020.s"
+.include "sound/songs/song021.s"
+.include "sound/songs/song022.s"
+.include "sound/songs/song023.s"
+.include "sound/songs/song024.s"
+.include "sound/songs/song025.s"
+.include "sound/songs/song026.s"
--- multi_boot/roms/collect_rings/sound/songs/song000.s
+++ multi_boot/roms/collect_rings/sound/songs/song000.s
@@ -2,11 +2,3 @@
 
 	.section .rodata
 
-	.align 2
-	.global song000
-song000:	@ 0x02016E64
-	.byte	0		@ trackCount
-	.byte	0		@ blockCount
-	.byte	0		@ priority
-	.byte	0		@ reverb
-
