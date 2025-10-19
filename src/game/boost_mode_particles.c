6910dfffa412917598dcbcbf2250ed1bab64a199
Oliver Bell <freshollie@gmail.com>
2025-10-19 10:32:49 +0100

    document boost mode particles

--- src/game/boost_mode_particles.c
+++ src/game/boost_mode_particles.c
@@ -16,12 +16,11 @@
 struct BoostModeParticles {
     Sprite unk0;
     Sprite unk30;
-    u16 unk60;
-    s16 unk62[NUM_PARTICLES][2];
-    s16 unkA2[NUM_PARTICLES][2];
+    u16 frame;
+    s16 positions[NUM_PARTICLES][2];
+    s16 accelerations[NUM_PARTICLES][2];
     s16 unkE2;
     s16 unkE4;
-    u16 fillerE6;
 };
 
 void sub_8089E54(void);
@@ -35,7 +34,7 @@ void CreateBoostModeParticles(void)
     struct BoostModeParticles *particles = TASK_DATA(t);
     Sprite *s = &particles->unk0;
 
-    particles->unk60 = 0;
+    particles->frame = 0;
     s->graphics.dest = VramMalloc(1);
     s->graphics.anim = SA2_ANIM_SONIC_BOOM_PARTICLES;
     s->variant = 0;
@@ -65,35 +64,35 @@ void CreateBoostModeParticles(void)
     for (i = 0; i < NUM_PARTICLES; i++) {
         u8 temp1;
         s32 rand, var;
-        particles->unk62[i][1] = (Random() & 0x7FF) + Q(16);
+        particles->positions[i][1] = (Random() & 0x7FF) + Q(16);
         if (gPlayer.moveState & MOVESTATE_FACING_LEFT) {
 #ifndef NON_MATCHING
             u32 z = (u32)gPlayer.rotation << 0x18;
             temp1 = (z + 0xC0000000) >> 0x18;
             asm("" ::: "memory");
 #else
-            temp1 = gPlayer.rotation + 0xC0;
+            temp1 = gPlayer.rotation + 192;
 #endif
-            particles->unk62[i][0] = Q_MUL((COS_24_8(((gPlayer.rotation + 128) & 0xFF) * 4)), particles->unk62[i][1]);
-            particles->unk62[i][1] = Q_MUL((SIN_24_8(((gPlayer.rotation + 128) & 0xFF) * 4)), particles->unk62[i][1]);
+            particles->positions[i][0] = Q_MUL(COS_24_8(((gPlayer.rotation + 128) & 0xFF) * 4), particles->positions[i][1]);
+            particles->positions[i][1] = Q_MUL(SIN_24_8(((gPlayer.rotation + 128) & 0xFF) * 4), particles->positions[i][1]);
 
         } else {
             temp1 = gPlayer.rotation + 64;
-            particles->unk62[i][0] = Q_MUL(COS_24_8(gPlayer.rotation * 4), particles->unk62[i][1]);
-            particles->unk62[i][1] = Q_MUL(SIN_24_8(gPlayer.rotation * 4), particles->unk62[i][1]);
+            particles->positions[i][0] = Q_MUL(COS_24_8(gPlayer.rotation * 4), particles->positions[i][1]);
+            particles->positions[i][1] = Q_MUL(SIN_24_8(gPlayer.rotation * 4), particles->positions[i][1]);
         }
 #ifndef NON_MATCHING
         {
             register s32 ip asm("ip");
             rand = ((s32(*)(void))Random)();
             ip = 0x3ff;
-            particles->unkA2[i][0] = Q_MUL(COS_24_8(temp1 * 4), (var = (rand & ip) + 512));
-            particles->unkA2[i][1] = Q_MUL(SIN_24_8(temp1 * 4), var);
+            particles->accelerations[i][0] = Q_MUL(COS_24_8(temp1 * 4), (var = (rand & ip) + 512));
+            particles->accelerations[i][1] = Q_MUL(SIN_24_8(temp1 * 4), var);
         }
 #else
         rand = Random();
-        particles->unkA2[i][0] = Q_MUL(COS_24_8(temp1 * 4), ((rand & 0x3FF) + 512));
-        particles->unkA2[i][1] = Q_MUL(SIN_24_8(temp1 * 4), var);
+        particles->accelerations[i][0] = Q_MUL(COS_24_8(temp1 * 4), ((rand & 0x3FF) + 512));
+        particles->accelerations[i][1] = Q_MUL(SIN_24_8(temp1 * 4), var);
 #endif
     }
 }
@@ -105,46 +104,46 @@ void sub_8089E54(void)
     Sprite *s;
     UpdateSpriteAnimation(&particles->unk0);
 
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < NUM_PARTICLES / 2; i++) {
         if (i & 1) {
-            particles->unk62[i][0] += particles->unkA2[i][0];
-            particles->unk62[i][1] += particles->unkA2[i][1];
+            particles->positions[i][0] += particles->accelerations[i][0];
+            particles->positions[i][1] += particles->accelerations[i][1];
         } else {
-            particles->unk62[i][0] -= particles->unkA2[i][0];
-            particles->unk62[i][1] -= particles->unkA2[i][1];
+            particles->positions[i][0] -= particles->accelerations[i][0];
+            particles->positions[i][1] -= particles->accelerations[i][1];
         }
 
-        particles->unkA2[i][0] = I(particles->unkA2[i][0] * 200);
-        particles->unkA2[i][1] = I(particles->unkA2[i][1] * 200);
+        particles->accelerations[i][0] = Q_MUL(particles->accelerations[i][0], Q(0.78125));
+        particles->accelerations[i][1] = Q_MUL(particles->accelerations[i][1], Q(0.78125));
         s = &particles->unk0;
-        s->x = I(gPlayer.qWorldX) - gCamera.x + I(particles->unk62[i][0]);
-        s->y = I(gPlayer.qWorldY) - gCamera.y + I(particles->unk62[i][1]);
+        s->x = I(gPlayer.qWorldX) - gCamera.x + I(particles->positions[i][0]);
+        s->y = I(gPlayer.qWorldY) - gCamera.y + I(particles->positions[i][1]);
         DisplaySprite(s);
     }
 
-    if (particles->unk60++ > 8) {
+    if (particles->frame++ > 8) {
         s->variant = 1;
         SeedRng(gPlayer.qWorldX, gCamera.x);
 
         for (i = 0; i < NUM_PARTICLES; i++) {
             u8 temp;
             s16 rand;
-            particles->unkE2 = 0x80;
+            particles->unkE2 = 128;
             particles->unkE4 = 0;
             if (gPlayer.moveState & MOVESTATE_FACING_LEFT) {
                 temp = Random();
                 temp += 64;
-                particles->unkE2 = (gSineTable[((gPlayer.rotation + 0x80) & 0xFF) * 4 + 0x100] >> 6) << 2;
-                particles->unkE4 = (gSineTable[((gPlayer.rotation + 0x80) & 0xFF) * 4] >> 6) << 2;
+                particles->unkE2 = COS_24_8(((gPlayer.rotation + 128) & 0xFF) * 4) * 4;
+                particles->unkE4 = SIN_24_8(((gPlayer.rotation + 128) & 0xFF) * 4) * 4;
             } else {
                 temp = Random();
-                particles->unkE2 = (gSineTable[(gPlayer.rotation * 4) + 0x100] >> 6) << 2;
-                particles->unkE4 = (gSineTable[gPlayer.rotation * 4] >> 6) << 2;
+                particles->unkE2 = COS_24_8(gPlayer.rotation * 4) * 4;
+                particles->unkE4 = SIN_24_8(gPlayer.rotation * 4) * 4;
             }
 
             rand = (Random() & 0x3FF);
-            particles->unkA2[i][0] = ((gSineTable[temp * 4 + 0x100] >> 6) * (rand + 0x600)) >> 8;
-            particles->unkA2[i][1] = ((rand + 0x600) * (gSineTable[temp * 4] >> 6)) >> 8;
+            particles->accelerations[i][0] = Q_MUL(COS_24_8(temp * 4), (rand + Q(6)));
+            particles->accelerations[i][1] = Q_MUL(SIN_24_8(temp * 4), (rand + Q(6)));
         }
 
         gCurTask->main = sub_808A0A4;
@@ -157,35 +156,35 @@ void sub_808A0A4(void)
     struct BoostModeParticles *particles = TASK_DATA(gCurTask);
     Sprite *s = &particles->unk0;
 
-    if (particles->unk60++ > 0x18) {
+    if (particles->frame++ > 24) {
         TaskDestroy(gCurTask);
         return;
     }
 
     for (i = 0; i < NUM_PARTICLES; i++) {
-        particles->unk62[i][0] += particles->unkA2[i][0];
-        particles->unk62[i][1] += particles->unkA2[i][1];
+        particles->positions[i][0] += particles->accelerations[i][0];
+        particles->positions[i][1] += particles->accelerations[i][1];
 
-        particles->unk62[i][0] -= particles->unkE2;
-        particles->unk62[i][1] -= particles->unkE4;
+        particles->positions[i][0] -= particles->unkE2;
+        particles->positions[i][1] -= particles->unkE4;
 
-        particles->unkA2[i][0] = (particles->unkA2[i][0] * 200) >> 8;
-        particles->unkA2[i][1] = (particles->unkA2[i][1] * 200) >> 8;
+        particles->accelerations[i][0] = Q_MUL(particles->accelerations[i][0], Q(0.78125));
+        particles->accelerations[i][1] = Q_MUL(particles->accelerations[i][1], Q(0.78125));
 
-        particles->unkE2 = (particles->unkE2 * 0x101) >> 8;
-        particles->unkE4 = (particles->unkE4 * 0x101) >> 8;
+        particles->unkE2 = Q_MUL(particles->unkE2, Q(1.00390625));
+        particles->unkE4 = Q_MUL(particles->unkE4, Q(1.00390625));
     }
 
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < (NUM_PARTICLES / 2); i++) {
         s = &particles->unk0;
-        if (particles->unk60 & 1) {
-            s->x = (I(gPlayer.qWorldX) - gCamera.x) + (particles->unk62[i][0] >> 8);
-            s->y = (I(gPlayer.qWorldY) - gCamera.y) + (particles->unk62[i][1] >> 8);
+        if (particles->frame & 1) {
+            s->x = (I(gPlayer.qWorldX) - gCamera.x) + I(particles->positions[i][0]);
+            s->y = (I(gPlayer.qWorldY) - gCamera.y) + I(particles->positions[i][1]);
             UpdateSpriteAnimation(s);
 
         } else {
-            s->x = (I(gPlayer.qWorldX) - gCamera.x) + (particles->unk62[i + 8][0] >> 8);
-            s->y = (I(gPlayer.qWorldY) - gCamera.y) + (particles->unk62[i + 8][1] >> 8);
+            s->x = (I(gPlayer.qWorldX) - gCamera.x) + I(particles->positions[i + (NUM_PARTICLES / 2)][0]);
+            s->y = (I(gPlayer.qWorldY) - gCamera.y) + I(particles->positions[i + (NUM_PARTICLES / 2)][1]);
         }
         DisplaySprite(s);
     }
