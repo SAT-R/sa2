981955eaae2b0417a29f9086c0007a1332fded5f
TsukiruP <104665617+TsukiruP@users.noreply.github.com>
2025-11-16 06:33:41 -0500

    Named Player_8013E34 (#229)
    * Name Player_8013E34
    
    * Update Task names
    
    * Renamed Player_SonicAmy_InitSkidAttackGfxTask to CreateSonicAmySkidAttackEffect.
    * Renamed Player_Sonic_Bound to CreateSonicBoundEffect.
    * Named sub_80129DC as CreateTailsTailSwipeEffect.
    * Named sub_8012DF8 as CreateKnucklesFireEffect.

--- include/game/player_callbacks.h
+++ include/game/player_callbacks.h
@@ -4,7 +4,7 @@
 #include "core.h"
 #include "game/sa1_sa2_shared/player.h"
 
-struct Task *Player_SonicAmy_InitSkidAttackGfxTask(s32 x, s32 y, u16 p2);
+struct Task *CreateSonicAmySkidAttackEffect(s32 x, s32 y, u16 p2);
 void Player_SonicAmy_InitSkidAttack(Player *);
 void Player_SonicAmy_InitStopNSlam(Player *);
 void Player_Sonic_InitHomingAttack(Player *);
