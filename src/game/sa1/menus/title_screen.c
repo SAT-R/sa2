239386354dc144d98b737eb1642e9b134fd6afca
JODY <128426924+JJODY4@users.noreply.github.com>
2026-04-27 19:44:39 +0100

    Update title_screen.c
    wasnt sure whether to name it based on how the gba hardware works or how the effect actually appears. Im mixed on this so feel free to reject

--- src/game/sa1/menus/title_screen.c
+++ src/game/sa1/menus/title_screen.c
@@ -146,8 +146,8 @@ void Task_SegaLogoInit(void)
     logo = TASK_DATA(gCurTask);
     gDispCnt |= DISPCNT_BG2_ON;
     logo->unk0++;
-    SA2_LABEL(sub_8007858)(2U, 0U, (160 - logo->unk0), gBgScrollRegs[2][0], (gBgScrollRegs[2][1] + 160) - logo->unk0);
-    SA2_LABEL(sub_80078D4)(2U, (160 - logo->unk0), 160, (u16)gBgScrollRegs[2][0], gBgScrollRegs[2][1]);
+    SA2_LABEL(Bg2_SetWindowRegionExtended)(2U, 0U, (160 - logo->unk0), gBgScrollRegs[2][0], (gBgScrollRegs[2][1] + 160) - logo->unk0);
+    SA2_LABEL(Bg2_SetWindowRegion)(2U, (160 - logo->unk0), 160, (u16)gBgScrollRegs[2][0], gBgScrollRegs[2][1]);
 
     if (logo->unk0 == 160) {
         gCurTask->main = sub_800D3E0;
@@ -155,14 +155,14 @@ void Task_SegaLogoInit(void)
     }
 }
 
-void Task_800D11C(void)
+void Task_SegaLogoScrollOut(void)
 {
     SegaLogo *logo;
 
     logo = TASK_DATA(gCurTask);
     logo->unk0++;
-    SA2_LABEL(sub_80078D4)(2U, 0U, (160 - logo->unk0), gBgScrollRegs[2][0], gBgScrollRegs[2][1]);
-    SA2_LABEL(sub_8007858)
+    SA2_LABEL(Bg2_SetWindowRegion)(2U, 0U, (160 - logo->unk0), gBgScrollRegs[2][0], gBgScrollRegs[2][1]);
+    SA2_LABEL(Bg2_SetWindowRegionEx)
     (2U, (160 - logo->unk0), 160, gBgScrollRegs[2][0], (gBgScrollRegs[2][1] + 160) - logo->unk0);
 
     if (logo->unk0 == 160) {
@@ -228,8 +228,8 @@ void Task_800D268(void)
 
     logo->unk0 += 2;
     if (logo->unk0 < 280) {
-        SA2_LABEL(sub_80078D4)(2, 0, 40, gBgScrollRegs[2][0], gBgScrollRegs[2][1]);
-        SA2_LABEL(sub_80078D4)(2, 120, 160, gBgScrollRegs[2][0], gBgScrollRegs[2][1]);
+        SA2_LABEL(Bg2_SetWindowRegion)(2, 0, 40, gBgScrollRegs[2][0], gBgScrollRegs[2][1]);
+        SA2_LABEL(Bg2_SetWindowRegion)(2, 120, 160, gBgScrollRegs[2][0], gBgScrollRegs[2][1]);
 
 #if !PORTABLE
         // BUG: For some reason this currently overwrites memory it shouldn't be able to access.
@@ -239,7 +239,7 @@ void Task_800D268(void)
             SA2_LABEL(sub_8007958)(2U, 40, 120, (s16)(280 - logo->unk0), -1, gBgScrollRegs[2][0], gBgScrollRegs[2][1]);
         } else {
             SA2_LABEL(sub_8007958)(2U, 40, (64 - logo->unk0), (280 - logo->unk0), -1, gBgScrollRegs[2][0], gBgScrollRegs[2][1]);
-            SA2_LABEL(sub_80078D4)(2U, (64 - logo->unk0), 120, gBgScrollRegs[2][0], gBgScrollRegs[2][1]);
+            SA2_LABEL(Bg2_SetWindowRegion)(2U, (64 - logo->unk0), 120, gBgScrollRegs[2][0], gBgScrollRegs[2][1]);
         }
 #endif
     } else {
