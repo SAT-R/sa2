010f2fa5ff1f3a2617c8aac76a259e442b00c0df
JODY <128426924+JJODY4@users.noreply.github.com>
2026-04-27 20:03:47 +0100

    Update items.h
    maybe this will allow make sa1 to work

--- include/constants/sa1/items.h
+++ include/constants/sa1/items.h
@@ -1,6 +1,3 @@
-#ifndef GUARD_CONSTANTS_ITEMS_H
-#define GUARD_CONSTANTS_ITEMS_H
-
 #if (GAME == GAME_SA1)
 #define ITEM__ONE_UP          0
 #define ITEM__SHIELD          1
@@ -13,15 +10,7 @@
 #define ITEM__MP_8            8
 #define ITEM__MP_9            9
 #define ITEM__MP_10           10
-
-#if (GAME == GAME_SA1)
-#define ITEM__COUNT 11
-#else
-#define ITEM__MP_11 11
-#define ITEM__MP_12 12
-
-#define ITEM__COUNT 13
-#endif
+#define ITEM__COUNT           11
 #elif (GAME == GAME_SA2)
 #define ITEM__ONE_UP          0
 #define ITEM__SHIELD          1
@@ -37,6 +26,7 @@
 #define ITEM__MP_ATTACK       11
 #define ITEM__MP_ATTACK_2     12
 #define ITEM__MP_RANDOM       13
-#endif
+#define ITEM__COUNT           13
+//#endif
 
 #endif // GUARD_CONSTANTS_ITEMS_H
